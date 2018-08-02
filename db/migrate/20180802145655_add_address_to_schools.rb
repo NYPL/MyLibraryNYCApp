5513f20b9dd62b5477242d4d36dfc572e04527bb
Lucas Cioffi <lucascioffi@nypl.org>
2018-08-02 10:59:37 -0400

    Add address to schools table MLN-127

---
+++ db/migrate/20180802145655_add_address_to_schools.rb
@@ -0,0 +1,10 @@
+class AddAddressToSchools < ActiveRecord::Migration
+  def change
+    add_column :schools, :address_line_1, :string
+    add_column :schools, :address_line_2, :string
+    add_column :schools, :state, :string
+    add_column :schools, :postal_code, :string
+    add_column :schools, :phone_numer, :string
+    add_column :schools, :borough, :string
+  end
+end
