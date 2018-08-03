11644e8dd47a370222001657aaef6aa4f90ab8c7
Lucas Cioffi <lucascioffi@nypl.org>
2018-08-03 14:29:09 -0400

    Rake task to import all NYC schools MLN-79

--- db/migrate/20180802145655_add_address_to_schools.rb
+++ db/migrate/20180802145655_add_address_to_schools.rb
@@ -4,7 +4,9 @@ class AddAddressToSchools < ActiveRecord::Migration
     add_column :schools, :address_line_2, :string
     add_column :schools, :state, :string
     add_column :schools, :postal_code, :string
-    add_column :schools, :phone_numer, :string
+    add_column :schools, :phone_number, :string
     add_column :schools, :borough, :string
+    add_column :schools, :created_at, :datetime
+    add_column :schools, :updated_at, :datetime
   end
 end
