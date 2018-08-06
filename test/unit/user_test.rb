1b192c2af5d99a84dbdb3bfd5fb132a53a62084b
Navraj Nat <nnat425@gmail.com>
2018-08-06 16:51:05 -0400

    remove variables

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,14 +3,6 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   include AwsDecrypt
 
-  # Create sample test data
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = generate_email
-  pin = [1, 1, 1, 1].map! { (0..9).to_a.sample }.join
-  school_id = '1076'
-  password = 'password123'
-
   setup do 
    @user = crank(:user)
   end 
