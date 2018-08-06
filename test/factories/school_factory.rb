39e9e47daa59f5a15bf8f0048fbe747273923542
Navraj Nat <nnat425@gmail.com>
2018-08-06 15:46:26 -0400

    generate random values

--- test/factories/school_factory.rb
+++ test/factories/school_factory.rb
@@ -2,9 +2,9 @@ class Cranky::Factory
   # Simple factory method to create a user instance, you would call this via crank(:user)
   def school
     School.new do |u|
-      u.id  = 9999
+      u.id  = [1,1,1,1].map!{|x| (0..9).to_a.sample}.join.to_i
       u.name  =   "Antonia Pantoja Preparatory Academy: A College Boar..."        
-      u.code  = 'zx999'              
+      u.code  = 'zx' + rand.to_s[2..4]             
       u.active =  true
     end
   end
