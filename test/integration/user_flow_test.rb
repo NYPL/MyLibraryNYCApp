34ca60478d4e8f38983e8b71b0cf91b1c9da178a
Navraj Nat <nnat425@gmail.com>
2018-08-08 13:45:07 -0400

    update tests

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -29,33 +29,41 @@ class UserFlowTest < ActionDispatch::IntegrationTest
   #   password_confirmation: password
   # )
 
+setup do 
+   @school = crank!(:school)
+   @user = crank!(:user, :school_id => @school.id)
+end
+
   # Test that generated barcode is a 14-digit string, starting with '27777'.
   test "user method assign_barcode increments last valid barcode by 1" do
-    new_user.assign_barcode
-    assert(new_user.reload.barcode == 27777000000001)
+    user_two = crank!(:user, :school_id => @school.id)
+    @user.assign_barcode
+    assert(@user.barcode == 27777099999999)
   end
 
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  email = generate_email
-  school = crank!(:school)
-  user = crank!(:user)
-
+  # first_name = Faker::Name.first_name
+  # last_name = Faker::Name.last_name
+  # email = generate_email
+  # school = crank!(:school)
+  # user = crank!(:user)
+[generate_email].each do |new_email|
   test 'create new user record and send request to microservice' do
     get '/users/signup'
     assert_select 'h1', 'Sign Up'
     post '/users',
     user: {
-      first_name: user.first_name,
-      last_name: user.last_name,
-      email: email,
-      pin: user.pin,
-      school_id: school.id
+      first_name: @user.first_name,
+      last_name: @user.last_name,
+      email: new_email,
+      pin: @user.pin,
+      school_id: @school.id
     }
+    puts response.body
     assert_response :redirect
     follow_redirect!
     assert 'div', 'Welcome! You have signed up successfully'
   end
+end 
 
 
   # Test that generated barcode is a 14-digit string, starting with '27777'.
