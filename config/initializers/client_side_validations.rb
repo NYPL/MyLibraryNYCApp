ebb04bad939012716f73934829687a1b7118cf82
Navraj Nat <nnat425@gmail.com>
2018-08-08 09:53:28 -0400

    fix accessibility issues on form

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -13,7 +13,7 @@
 #
  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
    unless html_tag =~ /^<label/
-     %{<div class="field_with_errors">#{html_tag}<label style='color:red' for="#{instance.send(:tag_id)}" class="message"> #{instance.error_message.first}</label></div>}.html_safe
+     %{<div class="field_with_errors">#{html_tag}<label style='color:#D13D44' for="#{instance.send(:tag_id)}" class="message"> #{instance.error_message.first}</label></div>}.html_safe
    else
      %{<div class="field_with_errors">#{html_tag}</div>}.html_safe
    end
