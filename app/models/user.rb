5bc701f6c23586f85766e125150ec63dda50b86a
Navraj Nat <nnat425@gmail.com>
2018-08-13 13:52:27 -0400

    add uniqueness back

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   attr_accessor :pin
 
   validates :school, :first_name, :last_name, :presence => true
-  validates :email, :uniqueness => true
+  validates :email, :alt_email, :uniqueness => true
   # Validation only occurs when a user record is being
   # created on sign up. Does not occur when updating
   # the record.
