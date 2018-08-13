1de3e8c04da003c31c0f1d1df7a9113875396b89
Navraj Nat <nnat425@gmail.com>
2018-08-13 10:56:45 -0400

    update javascript for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,9 @@ class User < ActiveRecord::Base
   # Makes getters and setters
   attr_accessor :pin
 
-  validates :school, :first_name, :last_name, :presence => true
+
+  validates :school_id, :first_name, :last_name, :presence => true
+  validates :email, :uniqueness => true
   # Validation only occurs when a user record is being
   # created on sign up. Does not occur when updating
   # the record.
