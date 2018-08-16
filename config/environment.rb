5227a4309188d8b8da6c150c4a86e01e4dde4618
Navraj Nat <nnat425@gmail.com>
2018-08-16 12:08:55 -0400

    change var name

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MyLibraryNYC::Application.initialize!
+
+mylibarynyc_log = ActiveSupport::BufferedLogger.new("log/my-library-nyc-application.log")
+MyLibraryNYC::Application = mylibarynyc_log
