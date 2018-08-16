616df5252f23d37f1625f047bb2efe0eaa9bfeb8
Navraj Nat <nnat425@gmail.com>
2018-08-16 12:41:01 -0400

    refactor code

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MyLibraryNYC::Application.initialize!
 
-mylibarynyc_log = ActiveSupport::BufferedLogger.new("log/my-library-nyc-application.log")
-MyLibraryNYC::Application = mylibarynyc_log
+Rails.logger = ActiveSupport::BufferedLogger.new("log/my-library-nyc-application.log")
