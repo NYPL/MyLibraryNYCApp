50f9f1d3a9967ec07dd33b33df4fbb3d565f991f
Lucas Cioffi <lucascioffi@nypl.org>
2018-08-23 10:14:34 -0400

    Delete extra migration file

--- db/migrate/20180820191047_ingest_all_nyc_schools.rb
+++
@@ -1,7 +0,0 @@
-class IngestAllNycSchools < ActiveRecord::Migration
-  def up
-    # Commenting this out so that we can run it after school codes are downcased in db/migrate/20180821150047_cleanup_school_codes.rb
-    # This will take effect on production, but QA will have already run this migration.
-    # Rake::Task['ingest:import_all_nyc_schools'].invoke('data/public/2016_-_2017_School_Locations.csv')
-  end
-end
