c98ccda69a3c0e80ed9f8234731dc4c47490e02e
Darya Chernikhova <daryachernikhova@nypl.org>
2018-08-24 11:46:29 -0400

    Added logging to hold_mailer, and made the email messages more nil-safe.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,20 @@
 class UserMailer < ActionMailer::Base
+  LOG_TAG = "UserMailer"
 
   default :from => "noreply@mylibrarynyc.org"
 
+  ##
+  # Let the user know they've successfully unsubscribed from regular notification emails.
+  # NOTE: Does not get used at this time.
   def unsubscribe(user)
-    @user = user
-    mail(:to => @user.contact_email, :subject => "You have now unsubscribed from MyLibraryNyc.")
+    begin
+      @user = user
+      Rails.logger.debug("#{LOG_TAG}.unsubscribe: About to send unsubscribe confirmation email to #{@user.email or 'unknown'}")
+      mail(:to => @user.contact_email, :subject => "You have now unsubscribed from MyLibraryNyc.")
+    rescue => exception
+      # something went wrong.  perhaps the user isn't set properly, or maybe the email couldn't be sent out.
+      Rails.logger.error("#{LOG_TAG}.unsubscribe: Cannot send notification email.  Backtrace=#{exception.backtrace}.")
+      raise
+    end
   end
 end
