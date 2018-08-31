b9d1ebd801636b3ea99910b185370beab63a0ee9
Lucas Cioffi <lucascioffi@nypl.org>
2018-08-31 15:22:06 -0400

    Send hold cancellation email even if no reason MLN-236

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -38,5 +38,5 @@ MyLibraryNYC::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
 
-  Rails.env = 'development'
+  Rails.env = 'local'
 end
