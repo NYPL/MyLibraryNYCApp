641c549a556661ab8195d5a59a94ac3f854d6512
Navraj Nat <nnat425@gmail.com>
2018-09-04 13:30:33 -0400

    fix bug

--- app/assets/javascripts/controllers/HoldNewCtrl.js
+++ app/assets/javascripts/controllers/HoldNewCtrl.js
@@ -38,6 +38,7 @@ app.controller('HoldNewCtrl', [ '$scope', '$routeParams', '$http', '$location', 
         });
       // Otherwise, just submit the form
       } else {
+        window.location = '/teacher_sets/' + $scope.teacher_set_id
         $scope.submitForm();
       }
     });
