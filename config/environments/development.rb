a61720bc0e70f086057e79de36ef302520480b62
Navraj Nat <nnat425@gmail.com>
2018-09-10 17:17:23 -0400

    print creds

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 MyLibraryNYC::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -38,6 +37,7 @@ MyLibraryNYC::Application.configure do
   config.action_mailer.delivery_method = :aws_sdk
 
   client = Aws::SES::Client.new(region: 'us-east-1')
+  puts client.config
 
   config.action_mailer.default_url_options = { :host => 'my-library-nyc-app-development.us-east-1.elasticbeanstalk.com' }
   config.action_mailer.perform_deliveries = true
