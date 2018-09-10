215d228a629cced088af7dd4f86260f1e404e88f
Navraj Nat <nnat425@gmail.com>
2018-09-10 17:21:22 -0400

    set default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,7 @@ MyLibraryNYC::Application.configure do
   client = Aws::SES::Client.new(region: 'us-east-1')
   puts client.config
 
+  config.action_mailer.default_options = {from: 'orders@mylibrarynyc.org'}
   config.action_mailer.default_url_options = { :host => 'my-library-nyc-app-development.us-east-1.elasticbeanstalk.com' }
   config.action_mailer.perform_deliveries = true
 
