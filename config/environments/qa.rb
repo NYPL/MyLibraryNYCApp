2e0b256c9b54e5183794ce8663a6d0dff3fb7801
Navraj Nat <nnat425@gmail.com>
2018-09-13 15:09:58 -0400

    remove space

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -1,6 +1,5 @@
 MyLibraryNYC::Application.configure do
   # host doesn't matter, it only matters that it exists (for dev and qa, for production it does matter)
-
   config.action_mailer.default_url_options = { host:  'my-library-nyc-app-qa.us-east-1.elasticbeanstalk.com' }
   config.action_mailer.perform_deliveries = true
 
