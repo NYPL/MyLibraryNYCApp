57a471b649b85c4eb009e4f39457abc6c6f8e64b
Navraj Nat <nnat425@gmail.com>
2018-09-13 15:06:51 -0400

    refactor code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,5 @@ MyLibraryNYC::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'sets.mylibrarynyc.org' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-   :port =>           '587',
-   :address =>        'email-smtp.us-east-1.amazonaws.com',
-   :user_name =>      ENV['SMTP_USERNAME'],
-   :password =>       ENV['SMTP_PASSWORD'],
-   :domain =>         'mylibrarynyc.org',
-   :authentication => :plain
-  }
 
 end
