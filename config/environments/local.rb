89b3b68a7a5795b088a402293a2ed72ae3ea749b
Navraj Nat <nnat425@gmail.com>
2018-09-18 16:32:48 -0400

    duplicate lines gone

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -22,8 +22,8 @@ MyLibraryNYC::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Turn off asset pipline information showing in logs 
-  config.assets.logger = false 
+  # # Turn off asset pipline information showing in logs 
+  # config.assets.logger = false 
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
@@ -41,5 +41,4 @@ MyLibraryNYC::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = false  
 
-  Rails.env = 'local'
 end
