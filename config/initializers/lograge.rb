6594e5b501780494eb07d8075babb03bc97e9a0e
Navraj Nat <nnat425@gmail.com>
2018-09-20 16:26:11 -0400

    Update logger (#135)
    * test
    
    * fix bug on cloudwatch
    
    * duplicate lines gone
    
    * change rails env to dev
    
    * add lograge
    
    * add lograge and logging
    
    * remove dev line
    
    * change branch back to dev
    
    * rename wrapper
    
    * change commented config
    
    * delete lines

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,7 @@
+MyLibraryNYC::Application.configure do
+  config.lograge.enabled = true
+  config.lograge.custom_options = lambda do |event|
+    {:host => event.payload[:host], :params => event.payload[:params],:level => event.payload[:level]}
+  end
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
+end 
