1d20b89dad83d3a69fc5c800d6d36037aa65b141
Navraj Nat <nnat425@gmail.com>
2018-10-12 12:17:59 -0400

    enable swagger route to work

--- config/application.rb
+++ config/application.rb
@@ -67,5 +67,12 @@ module MyLibraryNYC
 
     config.assets.initialize_on_precompile = false
 
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
