3e2464c41466a435aa3694c6fd3ccc0cbd426549
Navraj Nat <nnat425@gmail.com>
2018-10-22 13:10:11 -0400

    add email notification setting

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,7 +5,7 @@ class AdminUser < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :email_notifications, :remember_me
   # attr_accessible :title, :body
 
   def name
