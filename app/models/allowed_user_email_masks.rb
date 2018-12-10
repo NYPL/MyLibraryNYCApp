d198f1754581a66c953832ce5e7b82debac049c8
Darya Chernikhova <daryachernikhova@nypl.org>
2018-12-10 17:43:29 -0500

    added class docstring to the new allowed email-tracking class

--- app/models/allowed_user_email_masks.rb
+++ app/models/allowed_user_email_masks.rb
@@ -1,3 +1,12 @@
+
+## A "mask" is a regular expression that can be used to isolate/approve a string.
+# We're using just the simplest form of a mask, like: "@nypl.org is allowed now".
+# We're allowing the admin users to add email address masks (domains is another word that's not
+# perfectly accurate) in the form of "@nypl.org", "@schools.nyc.gov", etc., with the "@" sign included.
+# (We're hoping that including the "@" will help the Admin Users understand what is an email domain more clearly.)
+#
+# The email masks stored in this table will be used to restrict the email addresses that site
+# users (teachers) can sign up for MLN accounts with.
 class AllowedUserEmailMasks < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email_pattern, :active, :created_at, :updated_at
