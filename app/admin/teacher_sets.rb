8f370af217b9fd1c2887881876f5c3997d822219
Deepika Gonuguntla <consultdg@nypl.org>
2019-06-03 11:47:16 -0400

    rremoved available checkbox code

--- app/admin/teacher_sets.rb
+++ app/admin/teacher_sets.rb
@@ -83,14 +83,6 @@ ActiveAdmin.register TeacherSet do
     render partial: 'admin/history'
   end
 
-  # action_item only: [:show] do
-  #   render(partial: 'teacher_sets/availability_links_container', locals: { teacher_set: teacher_set, action: 'show' })
-  # end
-
-  # action_item only: [:show] do
-  #   render(partial: 'teacher_sets/edit_button_container', locals: { teacher_set: teacher_set })
-  # end
-
   member_action :make_available, method: :put do
     teacher_set = TeacherSet.find(params[:id])
     teacher_set.availability = 'available'
