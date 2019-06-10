f7dde1a58ede8b96c8c8c09da5cfd5eba3a25008
Deepika Gonuguntla <consultdg@nypl.org>
2019-06-10 12:03:08 -0400

    removed unwanted test cases

--- test/unit/teacher_set_test.rb
+++ test/unit/teacher_set_test.rb
@@ -10,20 +10,7 @@ class TeacherSetTest < MiniTest::Test
     @mintest_mock1 = MiniTest::Mock.new
     @mintest_mock2 = MiniTest::Mock.new
   end
-
-  it 'creating a teacher set does not create a version, because papertrail is turned off' do
-    # we turn it off this way in app/admin/teacher_sets.rb for creating new sets via the admin dashboard
-    teacher_set = crank!(:teacher_set)
-    assert PaperTrail::Version.count == 0
-  end
-
-  it 'updating a teacher set creates a version' do
-    teacher_set = crank!(:teacher_set)
-
-    teacher_set.update_attributes(title: 'Title2')
-    assert PaperTrail::Version.count == 1
-  end
-
+  
   #Calls Bibs service 
   #Calculates the total number of items and available items in the BIb service response
   describe 'test update available and total count method' do
