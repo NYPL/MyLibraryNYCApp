315824f9380e3d4f6787445b9ab71e1697a91440
Deepika Gonuguntla <consultdg@nypl.org>
2019-06-13 11:55:32 -0400

    added assert equal case

--- test/unit/teacher_set_test.rb
+++ test/unit/teacher_set_test.rb
@@ -16,7 +16,7 @@ class TeacherSetTest < MiniTest::Test
     it 'test creating a teacher set does not create a version, because papertrail is turned off' do
       # we turn it off this way in app/admin/teacher_sets.rb for creating new sets via the admin dashboard
       teacher_set = crank!(:teacher_set)
-      assert PaperTrail::Version.count == 0
+      assert_equal(PaperTrail::Version.count == 0)
     end
   end
 
@@ -25,7 +25,7 @@ class TeacherSetTest < MiniTest::Test
       teacher_set = crank!(:teacher_set)
 
       teacher_set.update_attributes(title: 'Title2')
-      assert PaperTrail::Version.count == 1
+      assert_equal(PaperTrail::Version.count == 1)
     end
   end
 
