a6e42a91cae71e3df54703de1215ade41062eaf1
Deepika Gonuguntla <consultdg@nypl.org>
2019-06-13 11:32:08 -0400

    updated existing test cases

--- test/unit/teacher_set_test.rb
+++ test/unit/teacher_set_test.rb
@@ -11,6 +11,24 @@ class TeacherSetTest < MiniTest::Test
     @mintest_mock2 = MiniTest::Mock.new
   end
   
+
+  describe 'creating a teacher set does not create a version, because papertrail is turned off' do
+    it 'test creating a teacher set does not create a version, because papertrail is turned off' do
+      # we turn it off this way in app/admin/teacher_sets.rb for creating new sets via the admin dashboard
+      teacher_set = crank!(:teacher_set)
+      assert PaperTrail::Version.count == 0
+    end
+  end
+
+  describe 'updating a teacher set creates a version' do
+    it 'test updating a teacher set creates a version' do
+      teacher_set = crank!(:teacher_set)
+
+      teacher_set.update_attributes(title: 'Title2')
+      assert PaperTrail::Version.count == 1
+    end
+  end
+
   #Calls Bibs service 
   #Calculates the total number of items and available items in the BIb service response
   describe 'test update available and total count method' do
