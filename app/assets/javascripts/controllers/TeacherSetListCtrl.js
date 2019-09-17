2b4783c01d91de4bea993b1a28232700f4fa392d
Deepika Gonuguntla <consultdg@nypl.org>
2019-09-17 13:16:01 -0400

    added  grades

--- app/assets/javascripts/controllers/TeacherSetListCtrl.js
+++ app/assets/javascripts/controllers/TeacherSetListCtrl.js
@@ -8,13 +8,12 @@ app.controller('TeacherSetListCtrl', [ '$scope', '$timeout', '$cookieStore', '$l
     // Grade filter supports Pre-k and K 
     // Pre-k value is -2
     // K value is -1
-    $scope.grades = [-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12];
+    $scope.grades = [-2,-1,1,2,3,4,5,6,7,8,9,10,11,12];
     $scope.min_grade = $scope.grades[0];
     $scope.max_grade = $scope.grades[$scope.grades.length-1];
     $scope.grade_values = [];
     $scope.grade_values[0] = $scope.teacher_sets.queryParams.grade_begin || $scope.min_grade;
     $scope.grade_values[1] = $scope.teacher_sets.queryParams.grade_end || $scope.max_grade;
-    $scope.grade_filter1 =  $scope.grade_values[0] == -2? 'Pre K' :  $scope.grade_values[0] == -1 ? 'K' :  $scope.grade_values[0]
 
     $scope.onGradeSliderStop = function(grade_begin, grade_end){
       $scope.teacher_sets.doGrades(grade_begin, grade_end);
