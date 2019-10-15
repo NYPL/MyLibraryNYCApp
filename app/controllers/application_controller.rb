f1e81ca50c99cf3de4b1a17b9b2039770f665e98
Darya Chernikhova <daryachernikhova@nypl.org>
2019-10-15 16:39:52 -0400

    uncommented-out payload code, it now appears to be ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,16 @@ class ApplicationController < ActionController::Base
   def append_info_to_payload(payload)
     super
     payload[:host] = request.host
-    #console.log('payload[:host]')
-    #case payload[:status]
-    #when 200
-    #  payload[:level] = "INFO"
-    #when payload[:status]  > 200 && payload[:status]  < 400
-    #  payload[:level] = "DEBUG"
-    #when payload[:status]  > 400
-    #  payload[:level] = "WARNING"
-    #when payload[:status]  >= 500
-    #  payload[:level] = "ERROR"
-    #end
+    case payload[:status]
+    when 200
+      payload[:level] = "INFO"
+    when payload[:status]  > 200 && payload[:status]  < 400
+      payload[:level] = "DEBUG"
+    when payload[:status]  > 400
+      payload[:level] = "WARNING"
+    when payload[:status]  >= 500
+      payload[:level] = "ERROR"
+    end
   end
 
 
