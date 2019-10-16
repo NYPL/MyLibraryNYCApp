1de8137ff366edcf6715d93ee320b874c575bff3
Darya Chernikhova <daryachernikhova@nypl.org>
2019-10-16 17:49:13 -0400

    old migration, ignore

--- db/migrate/20181015105316_rename_subjects_teacher_sets.rb
+++ db/migrate/20181015105316_rename_subjects_teacher_sets.rb
@@ -2,7 +2,7 @@ class RenameSubjectsTeacherSets < ActiveRecord::Migration
   def change
     rename_table :subjects_teacher_sets, :subject_teacher_sets
 
-    rename_index :subjects_teacher_sets, :index_subjects_teacher_sets_on_subject_id_and_teacher_set_id, :index_subject_teacher_sets_on_subject_id_and_teacher_set_id
+    #rename_index :subjects_teacher_sets, :index_subjects_teacher_sets_on_subject_id_and_teacher_set_id, :index_subject_teacher_sets_on_subject_id_and_teacher_set_id
     rename_index :subjects_teacher_sets, :index_subjects_teacher_sets_on_teacher_set_id, :index_subject_teacher_sets_on_teacher_set_id
   end
 end
