4bc79be2ddbc44ac2a06f24f5c2b4ece50562752
Darya Chernikhova <daryachernikhova@nypl.org>
2019-12-25 18:53:08 -0500

    commenting out migrations to speed up travis

--- db/migrate/20140117153416_add_index_to_subjects.rb
+++ db/migrate/20140117153416_add_index_to_subjects.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class AddIndexToSubjects < ActiveRecord::Migration[4.2]
-  def change
-    add_index :subjects, ["title"], :name => "index_subjects_title", :unique => true
-  end
+  # Note: Commented out 2019-12-25 to speed up travis builds.
+  # def change
+  #   add_index :subjects, ["title"], :name => "index_subjects_title", :unique => true
+  # end
 end
