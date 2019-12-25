cd5685c81bae1e5a4b9f146ef8eb67d189095e85
Darya Chernikhova <daryachernikhova@nypl.org>
2019-12-25 18:26:30 -0500

    ignore the school ingestion -- it takes a long time

--- db/migrate/20180821102147_ingest_sierra_code_zcode_matches.rb
+++ db/migrate/20180821102147_ingest_sierra_code_zcode_matches.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class IngestSierraCodeZcodeMatches < ActiveRecord::Migration[4.2]
-  def up
-    Rake::Task['ingest:overwrite_sierra_code_zcode_matches'].invoke('data/public/sierra_code_zcode_matches.csv')
-  end
+  # Note: Commented out 2019-12-25 to speed up travis builds.
+  # def up
+  #   Rake::Task['ingest:overwrite_sierra_code_zcode_matches'].invoke('data/public/sierra_code_zcode_matches.csv')
+  # end
 end
