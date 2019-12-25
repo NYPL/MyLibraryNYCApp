19daf9a656e930002cafe62698a9c0fa33252fa6
Darya Chernikhova <daryachernikhova@nypl.org>
2019-12-25 18:58:25 -0500

    commenting out migrations to speed up travis

--- db/migrate/20140117210644_add_available_counts_to_teacher_sets_table.rb
+++ db/migrate/20140117210644_add_available_counts_to_teacher_sets_table.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class AddAvailableCountsToTeacherSetsTable < ActiveRecord::Migration[4.2]
-  def change
-    add_column :teacher_sets, :available_copies, :integer
-    add_column :teacher_sets, :total_copies, :integer
-  end
+  # Note: Commented out 2019-12-25 to speed up travis builds.
+  # def change
+  #   add_column :teacher_sets, :available_copies, :integer
+  #   add_column :teacher_sets, :total_copies, :integer
+  # end
 end
