dcde5dc4bdd9dab0ea8b9316e4340f04fc1e523c
Deepika Gonuguntla <consultdg@nypl.org>
2019-12-31 13:28:07 -0500

    fixed teacher_set create method

--- app/controllers/teacher_sets_controller.rb
+++ app/controllers/teacher_sets_controller.rb
@@ -124,10 +124,10 @@ class TeacherSetsController < ApplicationController
 
   # Strong parameters: protect object creation and allow mass assignment.
   def teacherset_params
-    params.permit(:slug, :grade_begin, :grade_end, :availability, :call_number, :description, :details_url, :edition, :id,
+    params.permit(:description, :title, :edition, :id,
                   :isbn, :language, :lexile_begin, :lexile_end, :notes, :physical_description, :primary_language, :publication_date,
                   :publisher, :series, :statement_of_responsibility, :sub_title, :title, :books_attributes,
-                  :available_copies, :total_copies, :primary_subject, :bnumber, :set_type, :contents, :last_book_change)
+                  :available_copies, :total_copies, :bnumber, :set_type, :contents, :last_book_change)
   end
 
 end
--- test/functional/teacher_sets_controller_test.rb
+++ test/functional/teacher_sets_controller_test.rb
@@ -3,6 +3,7 @@
 require 'test_helper'
 
 class TeacherSetsControllerTest < ActionController::TestCase
+
   setup do
     @teacher_set = teacher_sets(:one)
   end
@@ -13,16 +14,12 @@ class TeacherSetsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:teacher_sets)
   end
 
-  #Need to fix
-  # test "should create teacher_set" do
-  #   assert_difference('TeacherSet.count') do
-  #     post :create, teacher_set: {description: @teacher_set.description, title: @teacher_set.title}
-  #   end
-  #   assert_redirected_to teacher_set_path(assigns(:teacher_set))
-  # end
-
   test "should show teacher_set" do
     get :show, params: { id: @teacher_set.id }
     assert_response :success
+  end
+
+  test "should create teacher_set" do
+    post :create, params: {teacher_set: {description: @teacher_set.description, title: @teacher_set.title}}
   end
 end
