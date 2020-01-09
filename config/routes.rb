70ca76cd360226bdc5985a32a150b272dca4bfb2
gonuguntla <30579702+gonuguntla@users.noreply.github.com>
2020-01-09 12:39:27 -0500

    Redirect all route url to maintenance banner page (#568)
    * added maintenance-banner code
    
    * updated maintenance text message in admin page
    
    * reverted
    
    * fixed rubocop
    
    * added styles for banner
    
    * added styles
    
    * Redirecting all routes to  same controller
    
    * updated root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 MyLibraryNYC::Application.routes.draw do
 
+  # When we launch the new Rails5 upgraded MLN app we we would like to turn the R3 version to just show a maintenance page.
+  # root :to => 'home#index'
+
+  root :to => 'general#maintenance_banner'
+  get '*path', to: 'general#maintenance_banner'
+
   devise_for :users, :path => "users", :path_names => { :sign_in => 'start', :sign_out => 'signout', :sign_up => 'signup' }, :controllers => { :registrations => :registrations, :sessions => :sessions }
 
   devise_scope :user do
