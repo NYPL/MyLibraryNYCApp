2f77eb4b926b0c59ff701faf29a69ee9f4af15d4
Deepika Gonuguntla <consultdg@nypl.org>
2020-01-21 15:48:36 -0500

    Rails app need to support multiple urls

--- config/routes.rb
+++ config/routes.rb
@@ -81,7 +81,10 @@ MyLibraryNYC::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+
+  # Support multiple domains
+  root :to => "home#index", :constraints => {:host => "dev-www.mylibrarynyc.local"}
+  root :to => "settings#index", :constraints => {:host => "dev-sets.mylibrarynyc.local"}
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
