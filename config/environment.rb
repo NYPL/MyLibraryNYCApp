e2869b910119c0b6cc092e7146d78bcb177d1d71
gonuguntla <30579702+gonuguntla@users.noreply.github.com>
2020-01-23 13:53:20 -0500

    fixed production logs (#577)
    development logs are testing

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MyLibraryNYC::Application.initialize!
-
-Rails.logger = Logger.new(STDOUT)
-Rails.logger.level = Logger::DEBUG
