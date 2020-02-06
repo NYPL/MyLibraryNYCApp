75ec06d1d2c40b2dab67c28324881fadf081afce
Deepika Gonuguntla <consultdg@nypl.org>
2020-02-06 11:06:16 -0500

    added scheme file

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191031152524) do
+ActiveRecord::Schema.define(version: 20200206150637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,7 +120,7 @@ ActiveRecord::Schema.define(version: 20191031152524) do
     t.string   "postal_code"
     t.string   "phone_number"
     t.string   "borough"
-    t.datetime "created_at"
+    t.datetime "created_at",                                null: false
     t.datetime "updated_at"
     t.index ["active"], name: "index_schools_on_active", using: :btree
     t.index ["campus_id"], name: "index_schools_on_campus_id", using: :btree
