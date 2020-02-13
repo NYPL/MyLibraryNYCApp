782ba8c07d83dd121d56ad0607ccccad832279c8
Deepika Gonuguntla <consultdg@nypl.org>
2020-02-13 13:07:14 -0500

    removed space

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -6,6 +6,7 @@ MyLibraryNYC::Application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.force_ssl = false
+
   config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
   config.logger.level = Logger::INFO
 end
