885fd386ca83855dff4c6fa2613ed95eb671e770
gonuguntla <30579702+gonuguntla@users.noreply.github.com>
2020-02-18 14:44:01 -0500

    [MLN-580] Added alphabet anchors to top of Schools list page (#602)
    * MLN-580 Added alphabet anchors to top of Schools list page
    
    * Added layout for schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class SchoolsController < ApplicationController
+  layout 'info_site/application'
   
   def index
-    @schools = School.active
-
+    # Group by first letter of the school.
+    @schools = School.active.group_by { |i| i.name[0] }
+    @schools_arr = []
+    @schools.each do |key, value|
+      school_hash = {}
+      # If school name is start with alphabet letter, school names will display under aplhabet anchor eg: 'A' Academy for Careers (12), Academy(22).
+      # If school name is not start with alphabet letter, school names will display under '#' anchor. eg: '#' 486 newyork school (86),  56test(234).
+      school_hash['schoolname_first_letter'] = key.match?(/[A-Za-z]/) ? key : '#'
+      school_hash['school_names'] = value.collect do |i|
+        code = i.code.present? ? i.code[1..-1].upcase : ""
+        "#{i.name} (#{code})"
+      end
+      @schools_arr << school_hash
+    end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @schools }
