a47791a0e4b96741d6a3f7a91e5a35e863bb0935
Deepika Gonuguntla <consultdg@nypl.org>
2020-02-20 13:58:50 -0500

    added negative test cases

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -3,9 +3,14 @@
 require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
+  NO_ROUTE_MATCHES = "No route matches"
 
   test "test for sets index page" do
     assert_routing "/", :controller => "home", :action => "index"
+  end
+
+  test "test for sets index page with sets hostname" do
+    assert_routing "http://#{ENV['MLN_SETS_SITE_URL']}", :controller => "home", :action => "index"
   end
 
   test "test for info-site index page" do
@@ -16,16 +21,44 @@ class RoutesTest < ActionDispatch::IntegrationTest
     assert_routing "http://#{ENV['MLN_INFO_SITE_URL']}/about/participating-schools", get_params('participating_schools')
   end
 
+  test "test for info-site participating-schools page with sets hostname" do
+    resp = assert_raises ActionController::RoutingError do
+      Rails.application.routes.recognize_path("http://#{ENV['MLN_SETS_SITE_URL']}/about/participating-schools")
+    end
+    assert_equal(true, resp.message.include?(NO_ROUTE_MATCHES))
+  end
+
   test "test for info-site about page" do
     assert_routing "http://#{ENV['MLN_INFO_SITE_URL']}/about/about-mylibrarynyc", get_params('about')
+  end
+
+  test "test for info-site about page with sets hostname" do
+    resp = assert_raises ActionController::RoutingError do
+      Rails.application.routes.recognize_path("http://#{ENV['MLN_SETS_SITE_URL']}/about/about-mylibrarynyc")
+    end
+    assert_equal(true, resp.message.include?(NO_ROUTE_MATCHES))
   end
 
   test "test for info-site contacts-links page" do
     assert_routing "http://#{ENV['MLN_INFO_SITE_URL']}/contacts-links", get_params('contacts')
   end
 
+  test "test for info-site contacts-links page with sets hostname" do
+    resp = assert_raises ActionController::RoutingError do
+      Rails.application.routes.recognize_path("http://#{ENV['MLN_SETS_SITE_URL']}/contacts-links")
+    end
+    assert_equal(true, resp.message.include?(NO_ROUTE_MATCHES))
+  end
+
   test "test for info-ste digital resources page" do
     assert_routing "http://#{ENV['MLN_INFO_SITE_URL']}/help/access-digital-resources", get_params('digital_resources')
+  end
+
+  test "test for info-site digital resources page page with sets hostname" do
+    resp = assert_raises ActionController::RoutingError do
+      Rails.application.routes.recognize_path("http://#{ENV['MLN_SETS_SITE_URL']}/help/access-digital-resources")
+    end
+    assert_equal(true, resp.message.include?(NO_ROUTE_MATCHES))
   end
 
   private
