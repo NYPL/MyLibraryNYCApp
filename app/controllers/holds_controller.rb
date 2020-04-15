04528092f574f86507c0ac82b5d9897443b60b61
daryanypl <daryachernikhova@nypl.org>
2020-04-15 01:36:42 -0400

    Merge: development -> qa.  Fixing double-rendering error in holds controller. (#626)
    * adding unique index to the users barcode column (#620)
    
    * added return statements after render json (#623)
    
    * added return statements after render json
    
    * tested and settled exactly where return statements were needed

--- app/controllers/holds_controller.rb
+++ app/controllers/holds_controller.rb
@@ -14,8 +14,11 @@ class HoldsController < ApplicationController
     redirect_to root_url
   end
 
-  
-  # GET /holds/1.json
+
+  # Performs the action of showing the detail of a single hold record, including
+  # its date, status, and teacher set.  Is usually called by the teacher when checking
+  # their holds history.  In routing terms, responds to a GET request on the
+  # /holds/[hold id].json url.
   def show
     LogWrapper.log('DEBUG', {'message' => 'show.start', 'method' => 'app/controllers/holds_controller.rb.show'})
 
@@ -29,7 +32,7 @@ class HoldsController < ApplicationController
     }, serializer: HoldExtendedSerializer, root: false
   end
 
-  
+
   # GET /holds/new.json
   def new
     LogWrapper.log('DEBUG', {'message' => 'new.start', 'method' => 'app/controllers/holds_controller.rb.new'})
@@ -41,7 +44,7 @@ class HoldsController < ApplicationController
     }
   end
 
-  
+
   # GET /holds/1/cancel.json
   def cancel
     LogWrapper.log('DEBUG', {'message' => 'cancel.start', 'method' => 'app/controllers/holds_controller.rb.cancel'})
@@ -54,19 +57,20 @@ class HoldsController < ApplicationController
     }
   end
 
-  
+
   ##
   # Create holds and update quantity column in holds.
   # Calculate available copies from quantity saves in teacherset table.
   def create
     LogWrapper.log('DEBUG', {'message' => 'create.start', 'method' => 'app/controllers/holds_controller.rb.create'})
-
     begin
-      # If currentuser school is inactive display error message and redirect to same page.
+      # If user's school is inactive, then display an error message and redirect to teacher set detail page.
       is_school_active = current_user.school_id.present? ? School.find(current_user.school_id).active : false
 
       if !is_school_active
         render json: {:redirect_to => "#{app_url}#/teacher_sets/#{params[:teacher_set_id]}", :is_school_active => is_school_active}
+        # stop processing further code, so new hold is not created,
+        # and double-render runtime error is not caused in the rescue exception block.
         return
       end
 
@@ -85,22 +89,25 @@ class HoldsController < ApplicationController
 
       respond_to do |format|
         if @hold.save
-          format.html { redirect_to hold_url(@hold.access_key), notice: 
+          LogWrapper.log('DEBUG', {'message' => 'create: a pre-existing hold was saved', 'method' => 'app/controllers/holds_controller.rb.create'})
+          format.html { redirect_to hold_url(@hold.access_key), notice:
             'Your order has been received by our system and will soon be delivered to your school.\
             <br/><br/>Check your email inbox for a message with further details.' }
           format.json { render json: @hold, status: :created, location: @hold }
         else
+          LogWrapper.log('DEBUG', {'message' => 'create: a new hold was generated', 'method' => 'app/controllers/holds_controller.rb.create'})
           format.html { render action: 'new' }
           format.json { render json: @hold.errors, status: :unprocessable_entity }
         end
       end
     rescue => exception
+      LogWrapper.log('ERROR', 'message' => exception.message)
+      # Note: don't need an explicit return here
       respond_to do |format|
         format.json {
           render json: { error: "We've encountered an error and were unable to confirm your order.\
             Please try again later or email help@mylibrarynyc.org for assistance.",
           rails_error_message: exception.message }.to_json, status: 500 }
-        LogWrapper.log('ERROR', 'message' => exception.message)
       end
     end
   end
