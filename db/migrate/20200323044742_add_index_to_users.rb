fbb13569efc11d431785de636bebd4b7c7f16043
Deepika Gonuguntla <consultdg@nypl.org>
2020-04-15 12:46:35 -0400

    reverted code

--- db/migrate/20200323044742_add_index_to_users.rb
+++ db/migrate/20200323044742_add_index_to_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class AddIndexToUsers < ActiveRecord::Migration[5.0]
-  # def change
-  #   change_column :users, :barcode, 'bigint'
-  #   add_index :users, :barcode, name: "index_users_barcode", unique: true
-  # end
+  def change
+    change_column :users, :barcode, 'bigint'
+    add_index :users, :barcode, name: "index_users_barcode", unique: true
+  end
 end
