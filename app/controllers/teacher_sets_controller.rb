4a3ba593387f3a29dca6700b0c4568719cefa67e
Deepika Gonuguntla <consultdg@nypl.org>
2020-04-20 16:01:59 -0400

    commented unwanted code

--- app/controllers/teacher_sets_controller.rb
+++ app/controllers/teacher_sets_controller.rb
@@ -60,7 +60,7 @@ class TeacherSetsController < ApplicationController
   end
 
   def index
-    create_teacherset_document_in_es
+    #create_teacherset_document_in_es
     LogWrapper.log('DEBUG', {'message' => 'index.start', 'method' => 'app/controllers/teacher_sets_controller.rb.index'})
   begin
     if true#MLNConfigurationController.new.feature_flag_config('dashboard.enabled')
