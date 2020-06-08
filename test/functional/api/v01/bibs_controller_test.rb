51b4664fb981851d3cdbba2d5b7671e0e32ab44b
Deepika Gonuguntla <consultdg@nypl.org>
2020-06-08 16:50:28 -0400

    reverted test case

--- test/functional/api/v01/bibs_controller_test.rb
+++ test/functional/api/v01/bibs_controller_test.rb
@@ -73,10 +73,9 @@ class Api::BibsControllerTest < ActionController::TestCase
   end
 
   test "should delete teacher sets when given a bib number" do
-    TeacherSet.where(bnumber: "b#{BNUMBER1}").first_or_create
-    # TODO: Need to fix
-    # delete 'delete_teacher_sets', params: { _json: TWO_TEACHER_SETS_TO_DELETE }
-    # assert_response :success
-    # assert JSON.parse(response.body)['teacher_sets'].map{ |x| x['bnumber'] } == ["b#{BNUMBER1}"]
+    teacher_set = TeacherSet.where(bnumber: "b#{BNUMBER1}").first_or_create
+    delete 'delete_teacher_sets', params: { _json: TWO_TEACHER_SETS_TO_DELETE }
+    assert_response :success
+    assert JSON.parse(response.body)['teacher_sets'].map{ |x| x['bnumber'] } == ["b#{BNUMBER1}"]
   end
 end
