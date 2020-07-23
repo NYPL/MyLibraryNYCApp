71a7abf48f9d7d66177dac5193e447cc349bcf68
Deepika Gonuguntla <consultdg@nypl.org>
2020-07-23 15:29:47 -0400

    added rubocop issues

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 module Exceptions
-
   # This class is created to throw exceptions if
   # we get an invalid response from the token or 
   # micro service. 
   class InvalidResponse < StandardError 
   end
-
 end
