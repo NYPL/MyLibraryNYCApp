e0ee091c11f369e23c2af5b051be09e2288d3a3c
Darya Chernikhova <daryachernikhova@nypl.org>
2020-08-03 16:22:39 -0400

    Added a sample ActiveJob

--- app/jobs/poke_model_1_job.rb
+++ app/jobs/poke_model_1_job.rb
@@ -2,6 +2,8 @@ class PokeModel1Job < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.logger.debug "#{self.class.name}: I'm performing my MLN2019 job with arguments: #{args.inspect}"
+    # Do something later
+    #User::DoSomethingAwesome.call(user)
+    Rails.logger.debug "#{self.class.name}: I'm performing my job with arguments: #{args.inspect}"
   end
 end
