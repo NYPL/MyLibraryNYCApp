19a3c9b727a673d6e59344d8f78848a308f3b391
Deepika Gonuguntla <consultdg@nypl.org>
2020-08-13 14:41:36 -0400

    removed unwanted code

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class FaqsController < ApplicationController
+
   def frequently_asked_questions
+    # Get all frequently asked questions by position ASC order.
     Faq.get_faqs   
   end
 end
--- app/models/faq.rb
+++ app/models/faq.rb
@@ -4,6 +4,7 @@ class Faq < ApplicationRecord
   acts_as_list :add_new_at => :bottom
   validates :position, uniqueness: true, allow_blank: false, allow_nil: false
 
+  # Reordering of Frequently asked question when faq is destroyed.
   before_destroy { |record| record.remove_from_list }
   scope :get_faqs, -> { order("position ASC") }
 end
