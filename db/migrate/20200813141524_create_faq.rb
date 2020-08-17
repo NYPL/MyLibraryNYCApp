6b36e233c011109a0449d9129c1f3cbf7d626420
Deepika Gonuguntla <consultdg@nypl.org>
2020-08-17 10:20:49 -0400

    added null condition

--- db/migrate/20200813141524_create_faq.rb
+++ db/migrate/20200813141524_create_faq.rb
@@ -3,8 +3,8 @@
 class CreateFaq < ActiveRecord::Migration[5.0]
   def change
     create_table :faqs do |t|
-      t.text :question
-      t.text :answer
+      t.text :question, :null => false
+      t.text :answer, :null => false
       t.integer :position
     end
   end
