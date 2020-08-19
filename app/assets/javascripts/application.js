1ea45f9717d63f1e39d13d3263e16ab941f328ff
Deepika Gonuguntla <consultdg@nypl.org>
2020-08-19 15:14:19 -0400

    added look up page styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,22 @@ $(document).ready(function() {
     $("#error_messages_id").hide();
   });
 });
+
+
+$(document).ready(function() {
+  $("#search_participating_school").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    var count = 0;
+    $("#participating_schools_id *").filter(function() {
+      if ($(this).text().toLowerCase().indexOf(value) > -1) count++;
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+
+    if(count == 0){
+      $("#schools_not_found").html("Schools are not found")
+    }
+    else {
+      $("#schools_not_found").html("")
+    }
+  });
+});
