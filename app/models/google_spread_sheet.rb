2e3ceab896e11ee9e145e7454e0edf013e440937
Deepika Gonuguntla <consultdg@nypl.org>
2020-10-08 14:13:04 -0400

    added error message

--- app/models/google_spread_sheet.rb
+++ app/models/google_spread_sheet.rb
@@ -11,6 +11,10 @@ class GoogleSpreadSheet
     session = GoogleDrive::Session.from_service_account_key(StringIO.new(google_credentials))
     @spreadsheet = session.spreadsheet_by_title("MLN newsletter signup requests - #{ENV['RAILS_ENV']}")
     @worksheet = @spreadsheet.worksheets.first
+    rescue StandardError => e
+    LogWrapper.log('ERROR', {'message' => "Error occcured while calling the google sheets. #{e.message}",
+                               'method' => 'google_sheet_client'})
+    raise "We've encountered an error and were unable to confirm your email"
   end
 
   
