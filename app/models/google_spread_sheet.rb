352ae10925612214dac7df9690e10f45d851e948
Deepika Gonuguntla <consultdg@nypl.org>
2020-10-08 14:19:28 -0400

    fixed rubocop issues

--- app/models/google_spread_sheet.rb
+++ app/models/google_spread_sheet.rb
@@ -11,9 +11,9 @@ class GoogleSpreadSheet
     session = GoogleDrive::Session.from_service_account_key(StringIO.new(google_credentials))
     @spreadsheet = session.spreadsheet_by_title("MLN newsletter signup requests - #{ENV['RAILS_ENV']}")
     @worksheet = @spreadsheet.worksheets.first
-    rescue StandardError => e
+  rescue StandardError => e
     LogWrapper.log('ERROR', {'message' => "Error occcured while calling the google sheets. #{e.message}",
-                               'method' => 'google_sheet_client'})
+                             'method' => 'google_sheet_client'})
     raise "We've encountered an error and were unable to confirm your email"
   end
 
