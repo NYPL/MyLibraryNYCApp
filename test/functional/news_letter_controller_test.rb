3f0f6a840825f864c199c0fcebfdb4e3a1391b8f
Deepika Gonuguntla <consultdg@nypl.org>
2020-10-14 17:04:20 -0400

    fixed rubocop issues and added new test cases for news-letter functionality

---
+++ test/functional/news_letter_controller_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# TODO: We had some functional tests that tested CRUD (create-update-delete)
+# functionality of the teacher sets controller.  They were deleted in
+# https://github.com/NYPL/MyLibraryNYCApp/pull/565/files, and need to be
+# brought back, with changes that reflect the new ActiveRecord version.
+
+class NewsLetterControllerTest < ActionController::TestCase
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+end
