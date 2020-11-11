751456f95df9184fe1a5b9f773c0d663a81fb00e
Darya Chernikhova <daryachernikhova@nypl.org>
2020-11-10 20:53:02 -0500

    changed todo message

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -13,12 +13,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-  # todo: integration: mock sierra response of barcode not found, make sure the user keeps its
+  # TODO: integration: mock sierra response of barcode not found, make sure the user keeps its
   # barcode and changes status and saves.
-  # todo: integration: sierra response of barcode found or exception, make sure user increments barcode and tries again.
-  # todo: if pass blank barcode to check_barcode_found_in_sierra, it returns false, so make sure
+  # TODO: integration: sierra response of barcode found or exception, make sure user increments barcode and tries again.
+  # TODO: if pass blank barcode to check_barcode_found_in_sierra, it returns false, so make sure
   # the calling code is responsible for checking barcode string to exist and be reasonable
-  # todo: if there was a problem, and the problem is resolved, user does save with new status and barcode
+  # TODO: if there was a problem, and the problem is resolved, user does save with new status and barcode
 
   [generate_barcode].each do |barcode|
     test 'sierra user can be found by barcode' do
