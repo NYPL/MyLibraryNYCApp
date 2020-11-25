4e6fb381f6cc6d8b13b53a609f40487f212fa67e
Darya Chernikhova <daryachernikhova@nypl.org>
2020-11-24 23:48:16 -0500

    added explanation note on rails version for ActiveJob

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
+  # NOTE: Below ideas are great, and very recommended, but require
+  # rails >= 5.2.  We keep these notes here as a reminder to revisit,
+  # next time we upgrade our rails version.
+  
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
-
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+
 end
