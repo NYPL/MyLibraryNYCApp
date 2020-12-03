8994665a6019b4b4251c02695e00e6cc237a4713
Deepika Gonuguntla <consultdg@nypl.org>
2020-12-03 15:55:18 -0500

    added test cases

--- test/unit/news_letter_controller_unit_test.rb
+++ test/unit/news_letter_controller_unit_test.rb
@@ -29,14 +29,29 @@ class NewsLetterControllerUnitTest < MiniTest::Test
   end
 
 
-  describe 'Test news-letter email is decrypted or not' do
-    it 'Decrypt news-letetr email' do
+  describe 'Test create news letter email in google_sheets method' do
+
+    # Case: 1
+    it 'Test Decrypt news-letetr email method' do
       email = 'test@ss.com'
       params = {key: "edededede"}
       @mintest_mock1.expect(:call, ['test@ss.com'])
       resp = nil
       @nl_controller.stub :news_letter_google_spread_sheet_emails, @mintest_mock1 do
-        EncryptDecryptString.stub :decrypt_string, [email], [params['key']] do
+        EncryptDecryptString.stub :decrypt_string, email, [params['key']] do
+          resp = @nl_controller.create_news_letter_email_in_google_sheets(params)
+        end
+      end
+      assert_equal(true, resp)
+    end
+    # Case: 2
+    it 'Test error while creating the news letter email in google sheets' do
+      email = 'test@ss1.com'
+      params = {key: "edededede"}
+      @mintest_mock1.expect(:call, ['test@ss.com'])
+      resp = nil
+      @nl_controller.stub :news_letter_google_spread_sheet_emails, @mintest_mock1 do
+        EncryptDecryptString.stub :decrypt_string, email, [params['key']] do
           resp = @nl_controller.create_news_letter_email_in_google_sheets(params)
         end
       end
