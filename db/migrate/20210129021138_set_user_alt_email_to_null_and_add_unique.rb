2cc2cfeae87465fa94f6ee83febbf22da8ab5d51
Darya Chernikhova <daryachernikhova@nypl.org>
2021-01-28 22:51:07 -0500

    update alt_email column on users to be null where it is an empty string, and add uniqueness constraint on that column

---
+++ db/migrate/20210129021138_set_user_alt_email_to_null_and_add_unique.rb
@@ -0,0 +1,6 @@
+class SetUserAltEmailToNull < ActiveRecord::Migration[5.0]
+  def change
+    execute "UPDATE users SET alt_email=NULL where alt_email=''"
+    add_index :users, :alt_email, name: "index_users_alt_email", unique: true
+  end
+end
