f0e3ccb946becd86ebf73593661840cc6109faaf
Deepika Gonuguntla <consultdg@nypl.org>
2021-02-26 12:50:12 -0500

    added rails require files

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,15 @@
 
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 require File.expand_path('../boot', __FILE__)
 
