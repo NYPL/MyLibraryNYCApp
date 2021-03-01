6d11fd1bfaca65b873f9756fd19b01f042e05639
Deepika Gonuguntla <consultdg@nypl.org>
2021-03-01 14:21:52 -0500

    added storage

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,7 @@ MyLibraryNYC::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.active_storage.service = :test
 
   # Raise exception on mass assignment protection for Active Record models
   # config.active_record.mass_assignment_sanitizer = :strict
