6bd20e5f89ac9cee061e2a2282f5559ac69ad5d1
Deepika Gonuguntla <consultdg@nypl.org>
2021-03-09 21:32:02 -0500

    Fixed failed test cases

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -91,7 +91,7 @@ class UserTest < ActiveSupport::TestCase
     test 'user model cannot be created with 4 of the same repeated digits as pin' do
       @user.pin = "1111"
       @user.save
-      assert_equal(@user.errors.messages[:pin],["does not meet our requirements. Please try again."])
+      assert_equal(@user.errors.messages[:pin],["PIN does not meet our requirements. Please try again."])
     end
   end
 
@@ -99,7 +99,7 @@ class UserTest < ActiveSupport::TestCase
     test 'user model cannot be created with alternate repeated digits as pin' do
       @user.pin = "1212"
       @user.save
-      assert_equal(@user.errors.messages[:pin],["does not meet our requirements. Please try again."])
+      assert_equal(@user.errors.messages[:pin],["PIN does not meet our requirements. Please try again."])
     end
   end
 
@@ -107,7 +107,7 @@ class UserTest < ActiveSupport::TestCase
     test 'user model cannot be created with 3 of the same repeated digits in a row as pin' do
       @user.pin = "0007"
       @user.save
-      assert_equal(@user.errors.messages[:pin],["does not meet our requirements. Please try again."])
+      assert_equal(@user.errors.messages[:pin],["PIN does not meet our requirements. Please try again."])
     end
   end
 
