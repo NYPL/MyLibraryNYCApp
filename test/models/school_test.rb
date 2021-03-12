7d504dfd421ff94e07a22af05ae00b222a94516f
Deepika Gonuguntla <consultdg@nypl.org>
2021-03-11 22:53:58 -0500

    Fixed test cases

--- test/models/school_test.rb
+++ test/models/school_test.rb
@@ -7,26 +7,27 @@ class SchoolTest < ActionController::TestCase
   setup do
     @school1 = schools(:school_one)
     @school3 = schools(:school_three)
+    @school4 = schools(:school_four)
   end
 
   test 'test all active schools' do
-    resp = School.active_schools
+    resp = School.active_schools_data
     assert_equal(3, resp.count)
   end
 
   test 'test inactive school' do
-    resp = School.inactive_school(@school1)
-    assert_equal("[INACTIVE] #{@school1.name} (#{@school1.code[1..-1].upcase})", resp[0])
-    assert_equal(@school1.id, resp[1])
+    resp = School.name_id(@school4)
+    assert_equal("[INACTIVE] #{@school4.name} (#{@school4.code[1..-1].upcase})", resp[0])
+    assert_equal(@school4.id, resp[1])
   end
 
   test 'test with school code value' do
-    resp = School.school_code(@school1)
+    resp = School.code(@school1)
     assert_equal(" (#{@school1.code[1..-1].upcase})", resp)
   end
 
   test 'test with-out school code value' do
-    resp = School.school_code(@school3)
+    resp = School.code(@school3)
     assert_empty(resp)
   end
 end
