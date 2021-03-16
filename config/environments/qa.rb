3fcf17c6d1ef3455610ed110505d8929cbd9e72a
Deepika Gonuguntla <consultdg@nypl.org>
2021-03-16 14:55:09 -0400

    add prod urls

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -9,6 +9,7 @@ MyLibraryNYC::Application.configure do
 
   config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
   config.logger.level = Logger::INFO
-  config.hosts = [ENV['MLN_INFO_SITE_HOSTNAME'], ENV['MLN_SETS_SITE_HOSTNAME']]
+  config.hosts = [ENV['MLN_INFO_SITE_HOSTNAME'], ENV['MLN_SETS_SITE_HOSTNAME'],
+                  "my-library-nyc-app-qa25.unpc66pkwp.us-east-1.elasticbeanstalk.com"]
   config.eager_load = true
 end
