9c40534b58962482c854c314eb4409eb2d886438
Deepika Gonuguntla <consultdg@nypl.org>
2021-04-12 13:48:36 -0400

    Fixed test cases

--- test/functional/teacher_sets_test.rb
+++ test/functional/teacher_sets_test.rb
@@ -28,4 +28,17 @@ class TeacherSetsTest < ActionController::TestCase
     @teacher_set.update_included_book_list(SIERRA_USER['data'][0])
     assert_response :success
   end
+
+
+  test "Update elastic search document in elastic-search" do
+    teacher_set = teacher_sets(:teacher_set_six)
+    resp = teacher_set.create_or_update_teacherset_document_in_es
+    assert_equal(teacher_set.id.to_s, resp['_id'])
+  end
+
+
+  test 'Create teacher_set in db and elastic-search' do
+    resp = TeacherSet.create_or_update_teacher_set(SIERRA_USER["data"][0])
+    assert_equal(SIERRA_USER["data"][0]['title'], resp.title)
+  end
 end
