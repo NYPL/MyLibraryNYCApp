f09498315182d3c058c40765652bbae85bc69c8d
Deepika Gonuguntla <consultdg@nypl.org>
2021-04-28 13:55:54 -0400

    Fixed rubocop issues

--- lib/bib_service_api.rb
+++ lib/bib_service_api.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module BibServiceApi
-	#Sends a request to the items microservice.
-  #Calling items service api by pagination, fetching 25 items by each call pushing into array.
-  #If its getting less than 25 items by items service call, we are not calling again.
-  def send_request_to_items_microservice(bibid,offset=nil,response=nil,items_hash={})
+  # Sends a request to the items microservice.
+  # Calling items service api by pagination, fetching 25 items by each call pushing into array.
+  # If its getting less than 25 items by items service call, we are not calling again.
+  def send_request_to_items_microservice(bibid,offset = nil,response = nil,items_hash = {})
     limit = 25
     offset = offset.nil? ? 0 : offset += 1
     request_offset = limit.to_i * offset.to_i
@@ -12,11 +14,12 @@ module BibServiceApi
       return items_hash, items_found
     else
       items_query_params = "?bibId=#{bibid}&limit=#{limit}&offset=#{request_offset}"
-      response = HTTParty.get(ENV['ITEMS_MICROSERVICE_URL_V01'] + items_query_params, headers: { 
-        'authorization' => "Bearer #{Oauth.get_oauth_token}", 'Content-Type' => 'application/json' }, timeout: 10)
+      response = HTTParty.get(ENV['ITEMS_MICROSERVICE_URL_V01'] + items_query_params, 
+                              headers: { 'authorization' => "Bearer #{Oauth.get_oauth_token}", 
+                                         'Content-Type' => 'application/json' }, timeout: 10)
       
       if response.code == 200 || items_hash['data'].present?
-        resp = (ENV['RAILS_ENV'] == 'test')? JSON.parse(response) : response
+        resp = ENV['RAILS_ENV'] == 'test' ? JSON.parse(response) : response
         items_hash['data'] ||= []
         items_hash['data'] << resp['data'] if resp['data'].present?
         items_hash['data'].flatten!
@@ -41,7 +44,8 @@ module BibServiceApi
         })
       end
     end
-    #Recursive call
+
+    # Recursive call
     send(__method__, bibid, offset, response, items_hash)
   end
 
@@ -49,7 +53,6 @@ module BibServiceApi
   # Sends a request to the bibs microservice.(Get bib response by bibid)
   # Sierra-bib-response-by-bibid-url: "{BIBS_MICROSERVICE_URL_V01}/nyplSource=#{SIERRA_NYPL}&id=#{bibid}"
   def send_request_to_bibs_microservice(bibid)
-  	binding.pry
     bib_query_params = "?nyplSource=#{SIERRA_NYPL}&id=#{bibid}"
     response = HTTParty.get(ENV['BIBS_MICROSERVICE_URL_V01'] + bib_query_params, headers: { 'Authorization' => "Bearer #{Oauth.get_oauth_token}", 
       'Content-Type' => 'application/json' }, timeout: 10)
@@ -62,7 +65,6 @@ module BibServiceApi
         'responseData' => response.message
       })
     elsif response.code == 404
-      items_hash = response
       LogWrapper.log('DEBUG', {
         'message' => "The bib service could not find with bibid=#{bibid}",
         'method' => 'send_request_to_bibs_microservice',
