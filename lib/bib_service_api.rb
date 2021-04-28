1d6958ce2dc723637866fcd9dff6cb802ee56ede
Deepika Gonuguntla <consultdg@nypl.org>
2021-04-28 14:00:00 -0400

    removed unwanted logs

--- lib/bib_service_api.rb
+++ lib/bib_service_api.rb
@@ -23,18 +23,6 @@ module BibServiceApi
         items_hash['data'] ||= []
         items_hash['data'] << resp['data'] if resp['data'].present?
         items_hash['data'].flatten!
-        LogWrapper.log('DEBUG', {
-          'message' => "Response from item services api",
-          'method' => 'send_request_to_items_microservice',
-          'status' => response.code,
-          'responseData' => response.message
-        })
-      elsif response.code == 404
-        items_hash = response
-        LogWrapper.log('DEBUG', {
-          'message' => "The items service could not find the Items with bibid=#{bibid}",
-          'method' => 'send_request_to_items_microservice',
-          'status' => response.code
         })
       else
         LogWrapper.log('ERROR', {
@@ -56,21 +44,7 @@ module BibServiceApi
     bib_query_params = "?nyplSource=#{SIERRA_NYPL}&id=#{bibid}"
     response = HTTParty.get(ENV['BIBS_MICROSERVICE_URL_V01'] + bib_query_params, headers: { 'Authorization' => "Bearer #{Oauth.get_oauth_token}", 
       'Content-Type' => 'application/json' }, timeout: 10)
-
-    if response.code == 200
-      LogWrapper.log('DEBUG', {
-        'message' => "Response from bib services api",
-        'method' => 'send_request_to_bibs_microservice',
-        'status' => response.code,
-        'responseData' => response.message
-      })
-    elsif response.code == 404
-      LogWrapper.log('DEBUG', {
-        'message' => "The bib service could not find with bibid=#{bibid}",
-        'method' => 'send_request_to_bibs_microservice',
-        'status' => response.code
-      })
-    else
+    unless [200, 404].include?(response.code)
       LogWrapper.log('ERROR', {
         'message' => "An error has occured when sending a request to the bibs service bibid=#{bibid}",
         'method' => 'send_request_to_bibs_microservice',
