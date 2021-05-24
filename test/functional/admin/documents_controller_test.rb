ad47732d7fcb34c20ebcfbb3929793dc07b4726f
Deepika Gonuguntla <consultdg@nypl.org>
2021-05-24 15:04:39 -0400

    removed pry and added document test cases

---
+++ test/functional/admin/documents_controller_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'minitest/stub_any_instance'
+
+module Admin
+  class DocumentsControllerTest < ActionController::TestCase
+
+  	setup do
+      @document = documents(:one)
+      sign_in AdminUser.create!(email: 'admin@example.com', password: 'password')
+    end
+
+
+    test "test index method" do
+      get :index
+      assert_equal("200", response.code)
+      assert_response :success
+    end
+
+
+    test "test show method" do
+      response = get :show, params: { id: @document.id }
+      assert_equal("200", response.code)
+      assert_response :success
+    end
+
+
+    test 'Test document form ' do
+      response = get :edit, params: { id: @document.id }
+      assert_equal("200", response.code)
+      assert_response :success
+    end
+
+
+    test 'create calendar_of_event in mln database' do
+      response = nil
+      params = {"event_type"=>"calendar_of_event", "file_name"=>"MylibraryNycCalenderevent123", "url"=>"https://docs.google.com/document/d/1iBzIYM_GG5OCXkuF4vKwSYRFaH3gd8Q_kuDrqT7Iu4U/edit"}
+      Document.stub_any_instance :google_document, "file" do
+        post :create, params: { document: params }
+      end
+      assert_response :redirect
+    end
+  end
+end
--- test/functional/admin/schools_controller_test.rb
+++ test/functional/admin/schools_controller_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'pry'
+
 module Admin
   class SchoolsControllerTest < ActionController::TestCase
 
