b2f0de3db791eddfc4bb42e7a14cec8011551211
Deepika Gonuguntla <consultdg@nypl.org>
2021-06-16 08:17:53 -0400

    Fixed faq

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import Faq from "../components/Faq";
 
 
 export default (
 	<Router>
 		<Switch>
 			<Route path="/" exact component={Home} />
+			<Route path="/faq" exact component={Faq} />
 		</Switch>
 	</Router>
 );
