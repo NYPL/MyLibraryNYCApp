8af3872d50b032ddb3bbdfedab43f0a5b72704ab
Deepika Gonuguntla <consultdg@nypl.org>
2021-06-16 10:02:08 -0400

    added help styles

--- app/javascript/components/Help.jsx
+++ app/javascript/components/Help.jsx
@@ -3,5 +3,11 @@ import { Layout, Menu } from "antd";
 
 
 export default () => (
-	<div>This is Help</div>
+	<div role="main">
+  <h1 class="pg--title">Help</h1>
+    Have a question about library cards, your account, or 
+    library staff visiting your school for professional development?
+    
+    For schools in The Bronx, Manhattan, and Staten Island, write to:
+	</div>
 );
