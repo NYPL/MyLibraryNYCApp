772de8f19e7bc9a731d297b77c7b77f2cf3c4096
Deepika Gonuguntla <consultdg@nypl.org>
2021-07-06 22:53:40 -0400

    Fixed  participating schools

--- app/javascript/components/ParticipatingSchools.jsx
+++ app/javascript/components/ParticipatingSchools.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+
 import AppBreadcrumbs from "./AppBreadcrumbs";
 import axios from 'axios';
-
+import {
+  Input,
+  SearchBar,
+  Select,
+} from '@nypl/design-system-react-components';
 
 
 
@@ -9,13 +14,15 @@ export default class ParticipatingSchools extends Component {
 
     constructor(props) {
       super(props);
-      this.state = { usersCollection: [] };
+      this.state = { schools: [], search_school: "" };
+      this.handleKeyUp = this.handleKeyUp.bind(this);
+      //this.filterData = this.filterData.bind(this);
     }
 
     componentDidMount() {
       axios.get('/schools')
         .then(res => {
-          this.setState({ usersCollection: res.data.schools });
+          this.setState({ schools: res.data.schools });
         })
         .catch(function (error) {
             console.log(error);
@@ -23,31 +30,56 @@ export default class ParticipatingSchools extends Component {
     }
 
 
-    Schools(){
-      return this.state.usersCollection.map((data, i) => {
-        return <ul>
-            <li className="schoolList">{data['alphabet_anchor']}</li>
-            {data['school_names'].map(item =>
-              <li key="{item}">{item}<br/></li>
-            )}
 
-          </ul>
+
+    Schools() {
+    
+      return this.state.schools.map((data, i) => {
+
+          let filteredSchools = data['school_names'].filter (
+            (school) => {
+              let value = this.state.search_school.trim().toLowerCase();
+
+              return school.toLowerCase().indexOf(value) !== -1
+            }
+          );
+
+        return <ul>
+                <li className="schoolList alphabet_anchor">{data['alphabet_anchor']}</li>
+
+                {filteredSchools.map(item =>
+                  <li key="{item}">{item}<br/></li>
+                )}
+              </ul>
 
       })
     }
+ 
+
+  handleKeyUp = event => {
+    this.setState({search_school: event.target.value});
+  }
+
+
 
     render() {
         return (
           <div>
             <AppBreadcrumbs />
             <div className="participating_schools_list">
-              <div className="participating_schools schoolList">Participating Schools</div>
-              <div className="school_header schoolList">Does your school participate in MyLibraryNYC?</div>
+              <div className="schoolList bold large">Participating Schools</div>
+              <div className="school_header schoolList bold">Does your school participate in MyLibraryNYC?</div>
               <div className="schoolList">Find your school using the following links:</div>
-              <div className="schoolList">
+              <div >
                 <a href="##">#</a> <a href="#A">A</a> <a href="#B">B</a> <a href="#C">C</a> <a href="#D">D</a> <a href="#E">E</a> <a href="#F">F</a> <a href="#G">G</a> <a href="#H">H</a> <a href="#I">I</a> <a href="#J">J</a> <a href="#K">K</a> <a href="#L">L</a> <a href="#M">M</a> <a href="#N">N</a> <a href="#O">O</a> <a href="#P">P</a> <a href="#Q">Q</a> <a href="#R">R</a> <a href="#S">S</a> <a href="#T">T</a> <a href="#U">U</a> <a href="#V">V</a> <a href="#W">W</a> <a href="#X">X</a> <a href="#Y">Y</a> <a href="#Z">Z</a>
               </div>
-              <div>or type the name of your school in the box below</div>
+              <div className="schoolList">or type the name of your school in the box below</div>
+               <input
+                  id="search_participating_school"
+                  type="text"
+                  className="school_inbox"
+                  onChange={this.handleKeyUp}
+                />
 
               {this.Schools()}
             </div>
