41d6bf8d1cf908650d96410ec5cbb373200d3839
Deepika Gonuguntla <consultdg@nypl.org>
2021-11-23 11:44:05 -0500

    changed file name

--- db/migrate/20211118185846_add_and_update_nov_2021_schools_to_mln.rb
+++ db/migrate/20211118185846_add_and_update_nov_2021_schools_to_mln.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddNov2021SchoolsToMln < ActiveRecord::Migration[5.0]
+class AddAndUpdateNov2021SchoolsToMln < ActiveRecord::Migration[5.0]
   def up
     Rake::Task['ingest:import_all_nyc_schools'].invoke('data/public/Nov_2021_updating_existing_school_names.csv', true)
     Rake::Task['ingest:import_all_nyc_schools'].invoke('data/public/Nov_2021_new_school_in_mln.csv', true)
