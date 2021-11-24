7fd58f6e3c5eb947c78746b86cb97f57a40356d9
Deepika Gonuguntla <consultdg@nypl.org>
2021-11-24 15:49:31 -0500

    removed unwanted pry

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -4,7 +4,6 @@ class ExceptionsController < ActionController::Base
   layout 'application'
 
   def render_error
-    binding.pry
     @hide_breadcrumbs = true
     @exception = env["action_dispatch.exception"]
     @status_code = ActionDispatch::ExceptionWrapper.new(env, @exception).status_code
