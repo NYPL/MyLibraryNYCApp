311337cba54e84c90254e1f39d3161fbfc9f40ae
Deepika Gonuguntla <consultdg@nypl.org>
2021-11-24 11:03:38 -0500

    Fixed active admin title

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
+  content :title => "Dashboard" do
     order_param = params[:order]
     order = nil
     pending_order = nil
