2500a6db1414fc193e86fdc233514ac6eef493f1
Deepika Gonuguntla <consultdg@nypl.org>
2021-11-24 14:44:48 -0500

    added logger code

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 MyLibraryNYC::Application.configure do
+  
   # host doesn't matter, it only matters that it exists (for dev and qa, for production it does matter)
   config.action_mailer.default_url_options = { host: ENV['MLN_INFO_SITE_HOSTNAME'] }
   config.action_mailer.perform_deliveries = true
 
   config.force_ssl = false
 
-  config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
+  # config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
+  config.logger = Logger.new(STDOUT)
   config.logger.level = Logger::INFO
+  config.assets.logger = true
+
   config.hosts = [ENV['MLN_INFO_SITE_HOSTNAME'], ENV['MLN_SETS_SITE_HOSTNAME'],
                   ENV['MLN_ENVIRONMENT_URL'], ENV['MLN_API_GATEWAY_URL']]
   config.eager_load = true
