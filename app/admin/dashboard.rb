30f368ffc87e0bba4c4bdaad2d58e78809c43b1c
Deepika Gonuguntla <consultdg@nypl.org>
2021-11-24 15:23:52 -0500

    removed proc

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,7 +2,7 @@
 
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => "Dashboard"
 
   content :title => "Dashboard" do
     order_param = params[:order]
