7d11d140d70c3b3036e057668643d9017af35325
Deepika Gonuguntla <consultdg@nypl.org>
2021-11-30 16:02:46 -0500

    added codes

--- app/models/sierra_code_zcode_match.rb
+++ app/models/sierra_code_zcode_match.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class SierraCodeZcodeMatch < ActiveRecord::Base
-  # attr_accessible :sierra_code, :zcode
+
+  def self.create_sierra_code_zcode(zcode, sierra_code)
+    sierra_obj = self.initialize_zcode(zcode)
+    sierra_obj.sierra_code = sierra_code if sierra_code.present?
+    sierra_obj.zcode = zcode if zcode.present?
+    sierra_obj.save!
+  end
+
+
+  def self.initialize_zcode(zcode)
+    SierraCodeZcodeMatch.where(zcode: zcode).first_or_initialize
+  end
 end
--- db/migrate/20211130194046_add_nov_2021_sierra_codes_to_mln.rb
+++ db/migrate/20211130194046_add_nov_2021_sierra_codes_to_mln.rb
@@ -1,4 +1,6 @@
-class AddNov2021SierraCodes < ActiveRecord::Migration[6.1]
+# frozen_string_literal: true
+
+class AddNov2021SierraCodesToMln < ActiveRecord::Migration[6.1]
   def up
     Rake::Task['ingest:import_sierra_codes_and_zcodes'].invoke('data/public/Nov_2021_new_sierra_codes_in_mln.csv', true)
   end
