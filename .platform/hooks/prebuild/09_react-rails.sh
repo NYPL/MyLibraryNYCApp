f3939065a4c0230448f27d48cf510352e5cf5095
gkallenberg <gregorykallenberg@nypl.org>
2022-02-24 16:32:23 -0500

    adjusted script for nodejs build due to errors

--- .platform/hooks/prebuild/09_react-rails.sh
+++ .platform/hooks/prebuild/09_react-rails.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+
 set -e
 
 # Install Node.js
 echo "Installing Node.js"
-yum-config-manager --enable epel
-yum -y install nodejs
+/usr/bin/yum-config-manager --enable epel
+/usr/bin/yum -y install nodejs
 
 # Install Yarn
 echo "Installing Yarn"
-wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo;
-yum -y install yarn;
+/user/bin/wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+/usr/bin/yum -y install yarn
 
 # Yarn install
-yarn
+YARN=$(which yarn)
+
+$YARN
