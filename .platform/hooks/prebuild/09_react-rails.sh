5a45daa791508ac9594501be0593487916aeba23
Deepika Gonuguntla <consultdg@nypl.org>
2022-03-01 20:03:55 -0500

    reverted

--- .platform/hooks/prebuild/09_react-rails.sh
+++ .platform/hooks/prebuild/09_react-rails.sh
@@ -4,14 +4,14 @@ set -e
 
 # Install Node.js
 echo "Installing Node.js"
-wget https://rpm.nodesource.com/setup_14.x
+/user/bin/wget https://rpm.nodesource.com/setup_14.x
 chmod 755 setup_14.x
 ./setup_14.x
 /usr/bin/yum -y install nodejs
 
 # Install Yarn
 echo "Installing Yarn"
-wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+/user/bin/wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 /usr/bin/yum -y install yarn
 
 # Yarn install
