a28e6577d8fa7ec49ec4b1841171734d57e8c9a7
Deepika Gonuguntla <consultdg@nypl.org>
2022-03-08 16:30:34 -0500

    Loading  images in react on rails partial methods

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,11 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 import ReactOnRails from 'react-on-rails';
