7fc10d8616551737971ec2b0943b71aa7b56374e
Deepika Gonuguntla <consultdg@nypl.org>
2022-03-09 08:40:00 -0500

    removed unwanted  code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,6 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
-require("channels")
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
