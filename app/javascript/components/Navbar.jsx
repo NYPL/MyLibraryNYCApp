e7786392e7128a64a423d74fb26e7a9aa44be2c0
Deepika Gonuguntla <consultdg@nypl.org>
2022-03-10 21:36:58 -0500

    Added Navbar ids

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -46,15 +46,13 @@ class AccountDetailsSubMenu extends Component {
   AccountOrderLink() {
     if (this.state.user_signed_in) {
       return <>
-      <li className="nav__submenu-item">
-          <ReactRouterLink className="nav-link-colors" to="/account_details">My Account & Orders</ReactRouterLink>
-      </li>
+        <li className="nav__submenu-item"><ReactRouterLink className="nav-link-colors" to="/account_details">My Account & Orders</ReactRouterLink></li>
         <li className="nav__submenu-item"><ReactRouterLink className="nav-link-colors" onClick={this.signOut}>Sign Out</ReactRouterLink></li>
       </>
     } else {
       return <>
-      <li className="nav__submenu-item" ><ReactRouterLink className="nav-link-colors" to="/signin">My Account & Orders</ReactRouterLink></li>
-      <li className="nav__submenu-item"><ReactRouterLink className="nav-link-colors" to="/signup">Not Registered ? Please Sign Up</ReactRouterLink></li>
+        <li className="nav__submenu-item" ><ReactRouterLink className="nav-link-colors" to="/signin">My Account & Orders</ReactRouterLink></li>
+        <li className="nav__submenu-item"><ReactRouterLink className="nav-link-colors" to="/signup">Not Registered ? Please Sign Up</ReactRouterLink></li>
       </>
     }
   }
@@ -62,7 +60,7 @@ class AccountDetailsSubMenu extends Component {
   render() {
     return (
       <Box>
-        <List type={ListTypes.Ordered} inline={false} noStyling={false} className="nav__submenu">
+        <List id="navbar-account-details" type={ListTypes.Ordered} inline={false} noStyling={false} className="nav__submenu">
           {this.signInAccountLinks()}
           {this.AccountOrderLink()}
         </List>
@@ -71,9 +69,6 @@ class AccountDetailsSubMenu extends Component {
   }
 }
 
-
-  
-  
 export default class Navbar extends Component {
 
   constructor(props) {
@@ -91,53 +86,53 @@ export default class Navbar extends Component {
     this.setState({ showAboutMenu: false });
   };
 
-showAccountSigninLink() {
-  if (this.state.user_signed_in) {
-    return <>
-      <ReactRouterLink to="/account_details" className="nav-link-colors" onMouseEnter={this.handleHover}>Account{' '}
-        <Icon size="small">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-            <path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/>
-          </svg>
-        </Icon>
-      </ReactRouterLink>
-    </>
-  } else {
-    return <>
-      <ReactRouterLink to="/signin" className="nav-link-colors" onMouseEnter={this.handleHover}>Sign In{' '}
-        <Icon size="small">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-            <path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/>
-          </svg>
-        </Icon>
-      </ReactRouterLink>
-    </>
+  showAccountSigninLink() {
+    if (this.state.user_signed_in) {
+      return <>
+        <ReactRouterLink to="/account_details" className="nav-link-colors" onMouseEnter={this.handleHover}>Account{' '}
+          <Icon id="account-arrow-drop-down-id" size="small">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+              <path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/>
+            </svg>
+          </Icon>
+        </ReactRouterLink>
+      </>
+    } else {
+      return <>
+        <ReactRouterLink to="/signin" className="nav-link-colors" onMouseEnter={this.handleHover}>Sign In{' '}
+          <Icon id="signin-arrow-drop-down-id" size="small">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+              <path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z"/>
+            </svg>
+          </Icon>
+        </ReactRouterLink>
+      </>
+    }
   }
-}
 
-render() {
-  return (
-   <div className="header-topWrapper">
-      <ReactRouterLink to="/">
-        <div className="header-logo"><img border="0" src={mlnLogoRed}/></div>
-      </ReactRouterLink>
-    
-      <List type="ul" inline noStyling className="header-buttons" className="float-right">
+  render() {
+    return (
+      <div id="mln-navbar-id" className="header-topWrapper">
+        <ReactRouterLink to="/">
+          <div className="header-logo"><img border="0" src={mlnLogoRed}/></div>
+        </ReactRouterLink>
+      
+        <List id="mln-navbar-list" type="ul" inline noStyling className="header-buttons" className="float-right">
 
-        <li><ReactRouterLink to="/teacher_set_data" className="nav-link-colors">Search Teacher Sets</ReactRouterLink></li>
+          <li><ReactRouterLink to="/teacher_set_data" className="nav-link-colors">Search Teacher Sets</ReactRouterLink></li>
 
-        <li><ReactRouterLink to="/contacts" className="nav-link-colors">Contacts</ReactRouterLink></li>
+          <li><ReactRouterLink to="/contacts" className="nav-link-colors">Contacts</ReactRouterLink></li>
 
-        <li><ReactRouterLink to="/faq" className="nav-link-colors">FAQs</ReactRouterLink></li>
+          <li><ReactRouterLink to="/faq" className="nav-link-colors">FAQs</ReactRouterLink></li>
 
-        <li><ReactRouterLink to="/participating-schools" className="nav-link-colors ">Participating Schools</ReactRouterLink></li>
+          <li><ReactRouterLink to="/participating-schools" className="nav-link-colors ">Participating Schools</ReactRouterLink></li>
 
-        <li className="nav__menu-item">
-          {this.showAccountSigninLink()}
-          { this.state.showAboutMenu && <AccountDetailsSubMenu userSignedIn={this.state.user_signed_in}/> }
-        </li>
-      </List>
-    </div>
-  );
+          <li className="nav__menu-item">
+            {this.showAccountSigninLink()}
+            { this.state.showAboutMenu && <AccountDetailsSubMenu userSignedIn={this.state.user_signed_in}/> }
+          </li>
+        </List>
+      </div>
+    );
   }
 };
