cb9ba9b06a6c98731d7f7e2bd68f75adf3c3a137
Deepika Gonuguntla <consultdg@nypl.org>
2022-03-14 13:24:33 -0400

    Fixed activeadmin javascript loads

--- app/javascript/packs/active_admin.js
+++ app/javascript/packs/active_admin.js
@@ -3,24 +3,6 @@
 import "../stylesheets/active_admin";
 import "@activeadmin/activeadmin";
 import "activeadmin_addons"
-
-
-function activateSchool(schoolId, activate) {
-  if (activate == true){
-    $('#activate-school-' + schoolId + '-container').hide();
-    $('#inactivate-school-' + schoolId + '-container').show();
-  } else {
-    $('#inactivate-school-' + schoolId + '-container').hide();
-    $('#activate-school-' + schoolId + '-container').show();
-  }
-};
-
-function makeAvailableTeacherSet(teacherSetId, make_available) {
-  if (make_available == true){
-    $('#make-available-teacher-set-' + teacherSetId + '-container').hide();
-    $('#make-unavailable-teacher-set-' + teacherSetId + '-container').show();
-  } else {
-    $('#make-unavailable-teacher-set-' + teacherSetId + '-container').hide();
-    $('#make-available-teacher-set-' + teacherSetId + '-container').show();
-  }
-};
+import active_admin_custom from './active_admin_custom'
+window.activateSchool = active_admin_custom.activateSchool
+window.makeAvailableTeacherSet = active_admin_custom.makeAvailableTeacherSet
