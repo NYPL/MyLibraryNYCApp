23f2af258afb6a7e2545a66d20dd15f27b45f455
Deepika Gonuguntla <consultdg@nypl.org>
2022-03-15 13:08:06 -0400

    Fixed re-ordable in faq

--- app/javascript/packs/active_admin.js
+++ app/javascript/packs/active_admin.js
@@ -3,6 +3,7 @@
 import "../stylesheets/active_admin";
 import "@activeadmin/activeadmin";
 import "activeadmin_addons"
+import "activeadmin_reorderable"
 import active_admin_custom from './active_admin_custom'
 window.activateSchool = active_admin_custom.activateSchool
 window.makeAvailableTeacherSet = active_admin_custom.makeAvailableTeacherSet
