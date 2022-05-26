a249747270a007d1947dc642d080008a199f0284
Deepika Gonuguntla <consultdg@nypl.org>
2022-05-26 16:31:16 -0400

    added puts

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -6,6 +6,7 @@ class ExceptionsController < ActionController::Base
   def render_error
     @hide_breadcrumbs = true
     @exception = request.env["action_dispatch.exception"]
+    puts "###########      #{ActionDispatch::ExceptionWrapper.new(request.env, @exception).status_code}.    #########"
     @status_code = ActionDispatch::ExceptionWrapper.new(request.env, @exception).status_code
   end
 end
