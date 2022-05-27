20f2d7fdc913d03e7d972f985a6fb94c8d126cfc
Deepika Gonuguntla <consultdg@nypl.org>
2022-05-27 15:12:25 -0400

    for testing

--- config/application.rb
+++ config/application.rb
@@ -54,10 +54,12 @@ module MyLibraryNYC
     config.encoding = "utf-8"
 
     # custom error pages
-    config.exceptions_app = lambda do |env|
-      puts "###########  #{ExceptionsController.action(:render_error).call(env)}    #########"
-      ExceptionsController.action(:render_error).call(env)
-    end
+    config.exceptions_app = ActionDispatch::PublicExceptions.new(Rails.public_path)
+    # lambda do |env|
+    #   ExceptionsController.action(:render_error).call(env)
+    # end
+
+
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
