4805d7fe7bb93f8ceff0b7f9e709bed44d170f57
Deepika Gonuguntla <consultdg@nypl.org>
2022-06-22 21:55:38 -0400

    Installed NPM lint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        browser: true,
+        node: true,
+        es6: true,
+        jest: true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
