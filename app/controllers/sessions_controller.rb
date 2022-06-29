5f1b5558e1d039192894b20131f48c4cc642cf00
Deepika Gonuguntla <consultdg@nypl.org>
2022-06-29 13:12:44 -0400

    Added session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: session_params[:email])
+    puts "**************  #{session["user_return_to"]}    ***************"
+
     if @user
      # sign_in :user, @user, bypass: true
       login!
