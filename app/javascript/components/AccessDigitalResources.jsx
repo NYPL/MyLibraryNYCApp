b70fdf46a98d709357b66e0660a1e7466373ac50
Deepika Gonuguntla <consultdg@nypl.org>
2022-07-25 13:06:20 -0400

    Fixed access digital resources

--- app/javascript/components/AccessDigitalResources.jsx
+++ app/javascript/components/AccessDigitalResources.jsx
@@ -1,7 +1,7 @@
 import React, { Component, useState } from 'react';
 import AppBreadcrumbs from "./AppBreadcrumbs";
 import axios from 'axios';
-import { Accordion, Link, List, Heading, CardHeading, Card, CardContent, DSProvider } from '@nypl/design-system-react-components';
+import { Accordion, Link, List, Heading, CardHeading, Card, CardContent, DSProvider, Text } from '@nypl/design-system-react-components';
 
 export default function AccessDigitalResources(props) {
 
@@ -13,17 +13,17 @@ export default function AccessDigitalResources(props) {
               label: "Databases",
               panel: (
                 <p>
-                  <br/>
-                  Access databases from anywhere with an internet connection by entering the barcode on the back of your MyLibraryNYC card."<br/><br/>
+                  <Text>Access databases from anywhere with an internet connection by entering the barcode on the back of your MyLibraryNYC card.</Text>
                     <a href="http://www.bklynlibrary.org/eresources">Brooklyn Public Library Articles and Databases</a>
-                     <div>Provides a list of databases for children, teens, and adults.</div><br/>
+                     <Text>Provides a list of databases for children, teens, and adults.</Text>
 
-                    <a href="http://www.nypl.org/collections/articles-databases">New York Public Library Databases</a><br/>
-                      Offers the option to limit searches by audience to children or teens/young adults.<br/><br/>
+                    <a href="http://www.nypl.org/collections/articles-databases">New York Public Library Databases</a>
+                      <Text>Offers the option to limit searches by audience to children or teens/young adults.</Text>
 
-                    <a href="https://www.queenslibrary.org/research/research-databases">Queens Public Library Databases</a><br/>
-                    Features a list of databases intended for schoolwork.<br/><br/>
-                    See your school librarian if you encounter technical issues or need further assistance. You may also <a href="http://www.mylibrarynyc.org/contacts-links">contact</a> your public library for help.<br/><br/>
+
+                    <a href="https://www.queenslibrary.org/research/research-databases">Queens Public Library Databases</a>
+                    <Text>Features a list of databases intended for schoolwork.</Text>
+                    <Text>See your school librarian if you encounter technical issues or need further assistance. You may also <a href="http://www.mylibrarynyc.org/contacts-links">contact</a> your public library for help.</Text>
                 </p>
               ),
             } ]}
@@ -33,11 +33,10 @@ export default function AccessDigitalResources(props) {
               label: "E-Books and More",
               panel: (
                 <p>
-                  <br/>
-                  Find titles for children and young adults in your public library’s ebook collection. <br/><br/>
-                  <a href="http://www.bklynlibrary.org/e-books-and-more">Brooklyn Public Library Ebooks</a><br/>
-                  <a href="http://www.nypl.org/ebooks">New York Public Library Ebooks</a><br/>
-                  <a href="https://www.queenslibrary.org/help/how-to-access-digital-media/eBooks">Queens Public Library Ebooks</a><br/><br/>
+                  <Text>Find titles for children and young adults in your public library’s ebook collection.</Text>
+                  <Text><a href="http://www.bklynlibrary.org/e-books-and-more">Brooklyn Public Library Ebooks</a></Text>
+                  <Text><a href="http://www.nypl.org/ebooks">New York Public Library Ebooks</a></Text>
+                  <Text><a href="https://www.queenslibrary.org/help/how-to-access-digital-media/eBooks">Queens Public Library Ebooks</a></Text>
                 </p>
               ),
             } ]}
@@ -48,24 +47,23 @@ export default function AccessDigitalResources(props) {
               label: "Other Resources",
               panel: (
                 <p>
-                  <br/>
-                  <a href="http://www.bklynlibrary.org/brooklyncollection/connections">Brooklyn Connections</a><br/>
-                  Gives rare access to original archival materials in Brooklyn Public Library's Brooklyn Collection while students complete customized, standards-based projects.<br/><br/>
+                  <Link style={{"text-decoration": "none"}} href="http://www.bklynlibrary.org/brooklyncollection/connections">Brooklyn Connections</Link>
+                  <Text >Gives rare access to original archival materials in Brooklyn Public Library's Brooklyn Collection while students complete customized, standards-based projects.</Text>
 
-                  <a href="http://www.nypl.org/blog/subject/7716">NYPL Common Core Resources</a><br/>
-                  Lists Common Core resources developed by teachers and librarians.<br/><br/>
+                  <Link style={{"text-decoration": "none"}} href="http://www.nypl.org/blog/subject/7716">NYPL Common Core Resources</Link>
+                  <Text>Lists Common Core resources developed by teachers and librarians.</Text>
 
-                  <a href="http://digitalcollections.nypl.org/">NYPL Digital Collections</a><br/>
-                  Contains over 800,000 digitized items from NYPL’s vast holdings. Many of them are primary source materials.<br/><br/>
+                  <Link style={{"text-decoration": "none"}} href="http://digitalcollections.nypl.org/">NYPL Digital Collections</Link>
+                  <Text>Contains over 800,000 digitized items from NYPL’s vast holdings. Many of them are primary source materials.</Text>
 
-                  <a href="https://www.queenslibrary.org/programs-activities/teens/homework-resources">Queens Public Library Homework Help</a><br/>
-                  Gathers trusted resources that help students tackle their homework.<br/><br/>
+                  <Link style={{"text-decoration": "none"}} href="https://www.queenslibrary.org/programs-activities/teens/homework-resources">Queens Public Library Homework Help</Link>
+                  <Text>Gathers trusted resources that help students tackle their homework.</Text>
 
-                  <a href="https://www.engageny.org/resource/empire-state-information-fluency-continuum">Empire State Information Fluency Continuum</a><br/>
-                  Identifies three information literacy standards, indicators for each standard, and the skills that students should develop by grades 2, 5, 8, and 12.<br/><br/>
+                  <Link style={{"text-decoration": "none"}} href="https://www.engageny.org/resource/empire-state-information-fluency-continuum">Empire State Information Fluency Continuum</Link>
+                  <Text>Identifies three information literacy standards, indicators for each standard, and the skills that students should develop by grades 2, 5, 8, and 12.</Text>
 
-                  <a href="http://schools.nyc.gov/Academics/CommonCoreLibrary/default.htm">NYDOE Common Core Library</a><br/>
-                  Provides Common Core-aligned tasks, professional development resources, and more.
+                  <Link style={{"text-decoration": "none"}} href="http://schools.nyc.gov/Academics/CommonCoreLibrary/default.htm">NYDOE Common Core Library</Link>
+                  <Text>Provides Common Core-aligned tasks, professional development resources, and more.</Text>
                 </p>
               ),
             }
