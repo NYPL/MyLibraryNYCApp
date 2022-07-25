ab06087ff9db550e0c4c55add29840d7ed4923ef
Deepika Gonuguntla <consultdg@nypl.org>
2022-07-25 16:49:50 -0400

    added link condition

--- app/javascript/components/Banner.jsx
+++ app/javascript/components/Banner.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
 import "../styles/application.scss"
-import { Notification } from '@nypl/design-system-react-components';
+import { Notification, Link } from '@nypl/design-system-react-components';
 
 function Banner() {
   return (
     <Notification ariaLabel="Banner Notification" id="banner-notification" className="bannerMessage" isCentered showIcon={false} dismissible notificationType="announcement" notificationContent={<>
       We are now accepting teacher set returns. Teacher set deliveries are still currently suspended. Please email 
-    <a href="mailto:help@mylibrarynyc.org"> help@mylibrarynyc.org </a> for all general questions and 
-    <a href="mailto:delivery@mylibrarynyc.org"> delivery@mylibrarynyc.org </a> for all delivery questions.</>} />
-
+    <Link href="mailto:help@mylibrarynyc.org"> help@mylibrarynyc.org </Link> for all general questions and 
+    <Link href="mailto:delivery@mylibrarynyc.org"> delivery@mylibrarynyc.org </Link> for all delivery questions.</>} />
   );
 }
 
