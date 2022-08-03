5d98023f83e3594c4412987b17131ab32836352e
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-03 10:25:55 -0400

    removed logout  in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   skip_before_action :verify_authenticity_token
 
-  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!, :set_user
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :set_user
 
   def login!
     session[:user_id] = @user.id
