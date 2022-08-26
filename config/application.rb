b8615505b142ad7e98d7ea101c84d33e3d1d4be3
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-26 11:03:32 -0400

    Fixed rackcor credentilas true issue

--- config/application.rb
+++ config/application.rb
@@ -89,8 +89,7 @@ module MyLibraryNYC
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :put, :patch, :delete, :head],
-        credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :patch, :delete, :head]
       end
     end
 
