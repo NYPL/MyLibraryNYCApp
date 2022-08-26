83ca41d096ea01a647dd02558b15ba656c3b2f32
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-26 11:28:01 -0400

    Fixed session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,9 +2,18 @@
 
 # Be sure to restart your server when you modify this file.
 
-MyLibraryNYC::Application.config.session_store :cookie_store, key: '_MyLibraryNYC_session'
+# MyLibraryNYC::Application.config.session_store :cookie_store, key: '_MyLibraryNYC_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # MyLibraryNYC::Application.config.session_store :active_record_store
+
+
+Rails.application.config.session_store :cookie_store, {
+  :key => '_MyLibraryNYC',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 2
+}
