78f0c8505afbd83dc7376005f1dd44d2c095d9ad
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-26 10:09:54 -0400

    for  testing added middle ware cors

--- config/application.rb
+++ config/application.rb
@@ -89,7 +89,8 @@ module MyLibraryNYC
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :put, :patch, :delete, :head],
+        credentials: true
       end
     end
 
