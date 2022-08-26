342cead4aa1fecea794321b39faf777ce37e5594
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-26 15:29:02 -0400

    added url for qa

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -8,7 +8,15 @@ MyLibraryNYC::Application.configure do
   config.consider_all_requests_local = false
   config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
   config.logger.level = Logger::INFO
+  
+  config.session_store :cookie_store, key: '_interslice_session'
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use config.session_store, config.session_options
+
   config.hosts = [ENV['MLN_INFO_SITE_HOSTNAME'], ENV['MLN_SETS_SITE_HOSTNAME'],
-                  ENV['MLN_ENVIRONMENT_URL'], ENV['MLN_API_GATEWAY_URL']]
+                  ENV['MLN_ENVIRONMENT_URL'], ENV['MLN_API_GATEWAY_URL'],
+                  "http://my-library-nyc-app-react-qa-27.unpc66pkwp.us-east-1.elasticbeanstalk.com",
+                  "my-library-nyc-app-react-qa-27.unpc66pkwp.us-east-1.elasticbeanstalk.com"
+                ]
   config.eager_load = true
 end
