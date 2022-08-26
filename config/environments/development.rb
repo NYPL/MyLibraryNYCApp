668ba2bf4e0d3f161810395acb665e6cb08873dc
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-26 13:45:40 -0400

    Testing with session issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ MyLibraryNYC::Application.configure do
   config.action_mailer.default_url_options = { :host => ENV['MLN_INFO_SITE_HOSTNAME'] }
   config.action_mailer.perform_deliveries = false
 
-  config.session_store :cookie_store, key: '_MyLibraryNYC_session'
+  config.session_store :cookie_store, key: '_interslice_session'
   config.middleware.use ActionDispatch::Cookies
   config.middleware.use config.session_store, config.session_options
 
