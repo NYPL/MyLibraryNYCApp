253e2585ad9259e16d47dad7c7463ed94a2700c7
Deepika Gonuguntla <consultdg@nypl.org>
2022-08-30 00:43:30 -0400

    commented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery except: [:create]
-  protect_from_forgery only: [:update]
+  #protect_from_forgery only: [:update]
  # before_action :authenticate_user!
   #before_action :configure_permitted_parameters, if: :devise_controller?
 
