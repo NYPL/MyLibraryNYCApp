63859c3f305801ee29613d1d2ee44d28c52d5694
Deepika Gonuguntla <consultdg@nypl.org>
2022-09-05 17:25:41 -0400

    reveretd to original

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery only: [:update, :get, :put, :delete, :post, :patch]
+  protect_from_forgery only: [:update, :get, :put, :delete]
 
   def logged_in?
     if user_signed_in?
