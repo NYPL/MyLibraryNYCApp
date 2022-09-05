d73b26ba00d53de22016c6a2b2f66cf243041da0
Deepika Gonuguntla <consultdg@nypl.org>
2022-09-05 15:48:09 -0400

    added exception in app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: false
 
 class ApplicationController < ActionController::Base
-
-  protect_from_forgery except: [:create]
-  protect_from_forgery only: [:update, :get, :put, :delete, :post]
+  protect_from_forgery with: :exception
+  # protect_from_forgery except: [:create]
+  # protect_from_forgery only: [:update, :get, :put, :delete, :post]
 
   def logged_in?
     if user_signed_in?
