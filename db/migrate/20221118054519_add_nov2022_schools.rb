a554861058e34a3d670d9214d0429c2830987ae6
Deepika Gonuguntla <consultdg@nypl.org>
2022-11-18 12:21:24 +0530

    Added new schools and sierra codes in MLN

---
+++ db/migrate/20221118054519_add_nov2022_schools.rb
@@ -0,0 +1,6 @@
+class AddNov2022Schools < ActiveRecord::Migration[6.1]
+  def up
+    Rake::Task['ingest:import_all_nyc_schools'].invoke('data/public/Nov_2022_schools_in_mln.csv', true)
+    Rake::Task['ingest_sierra_data:import_sierra_codes_and_zcodes'].invoke('data/public/Nov_2022_sierra_codes_in_mln.csv', true)
+  end
+end
