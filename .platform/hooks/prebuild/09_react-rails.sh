27ba6d9ffc8171b1f0851c74b39b28d8e7115a95
Deepika Gonuguntla <consultdg@nypl.org>
2022-12-08 14:58:44 +0530

    Added webpack configs

--- .platform/hooks/prebuild/09_react-rails.sh
+++ .platform/hooks/prebuild/09_react-rails.sh
@@ -1,20 +1,29 @@
-# #!/usr/bin/bash
+#!/usr/bin/bash
 
-# set -e
+set -e
 
-# # Install Node.js
-# echo "Installing Node.js"
-# /usr/bin/wget https://rpm.nodesource.com/setup_14.x
-# chmod 755 setup_14.x
-# ./setup_14.x
-# /usr/bin/yum -y install nodejs
+# Install Node.js
+echo "Installing Node.js"
+/usr/bin/wget https://rpm.nodesource.com/setup_14.x
+chmod 755 setup_14.x
+./setup_14.x
+/usr/bin/yum -y install nodejs
 
-# # Install Yarn
-# echo "Installing Yarn"
-# /usr/bin/wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-# /usr/bin/yum -y install yarn
+# Install Yarn
+echo "Installing Yarn"
+/usr/bin/wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+/usr/bin/yum -y install yarn
 
-# # Yarn install
-# YARN=$(which yarn)
+# Yarn install
+YARN=$(which yarn)
 
-# $YARN
+$YARN
+
+08_install_weback:
+command: "sudo npm install --save-dev webpack"
+09_install_babel:
+command: "sudo npm install --save-dev babel-loader"
+10_webpack:
+command: "bundle exec rails webpacker:install"
+11_asset_precompile:
+command: "bin/webpack"
