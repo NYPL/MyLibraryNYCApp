210f3de89b81064a20ce02f35dc5ac6391617ae4
Deepika Gonuguntla <consultdg@nypl.org>
2023-03-23 09:40:21 -0400

    Added

--- test/factories/school_factory.rb
+++ test/factories/school_factory.rb
@@ -5,17 +5,17 @@ class Cranky::Factory
     define name: "Queens Academy: College Prep",
            address_line_1: '100 Main St',
            address_line_2: 'Example, NY 10000',
-           code: 'z' + unique_school_code,
+           code: "z#{unique_school_code}",
            borough: 'QUEENS',
            active: true
   end
 
   def queens_school
-    inherit(:school, code: 'zq' + unique_school_code, borough: 'QUEENS')
+    inherit(:school, code: "zq#{unique_school_code}", borough: 'QUEENS')
   end
 
   def bronx_school
-    inherit(:school, code: 'zx' + unique_school_code, borough: 'BRONX')
+    inherit(:school, code: "zx#{unique_school_code}", borough: 'BRONX')
   end
 
   def unique_school_code
