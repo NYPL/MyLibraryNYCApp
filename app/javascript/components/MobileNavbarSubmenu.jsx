0a96dea3b1d7bfd80e8d325a661084185357bdac
Deepika Gonuguntla <consultdg@nypl.org>
2023-04-24 10:41:39 -0400

    Fixed lint issues

--- app/javascript/components/MobileNavbarSubmenu.jsx
+++ app/javascript/components/MobileNavbarSubmenu.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import {
   Link,
@@ -89,5 +89,4 @@ export default function MobileNavbarSubmenu() {
       </List>
     </div>
   );
-
 }
