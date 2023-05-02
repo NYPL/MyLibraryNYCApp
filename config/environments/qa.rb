dff56a886ea40fd676592c3d0b4f5ddeb53fada5
Deepika Gonuguntla <consultdg@nypl.org>
2023-05-02 14:47:46 -0400

    Added logs

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -6,7 +6,8 @@ MyLibraryNYC::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.force_ssl = false
   config.consider_all_requests_local = false
-  config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
+  #config.logger = ActiveSupport::Logger.new("log/my-library-nyc-application.log")
+  config.logger = Logger.new($stdout)
   config.logger.level = Logger::INFO
   
   # config.session_store :cookie_store, key: '_interslice_session'
