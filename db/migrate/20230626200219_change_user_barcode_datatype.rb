f659f136aba106fb451da7dba148b154b078b980
Deepika Gonuguntla <consultdg@nypl.org>
2023-09-08 16:30:40 -0400

    Fixed rubocop issues

--- db/migrate/20230626200219_change_user_barcode_datatype.rb
+++ db/migrate/20230626200219_change_user_barcode_datatype.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeUserBarcodeDatatype < ActiveRecord::Migration[6.1]
   def change
     change_column :users, :barcode, 'bigint', null: false, unique: true
