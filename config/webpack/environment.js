5c3ef36c47e0211cdae60494f83588e2c76310cc
Deepika Gonuguntla <consultdg@nypl.org>
2023-09-21 16:06:52 -0400

    Added web-dev-server latest version

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-const jquery = require('./plugins/jquery')
 
-environment.plugins.prepend('jquery', jquery)
 module.exports = environment
