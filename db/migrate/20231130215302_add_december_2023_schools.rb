9290ac53c004f4a5951fc41dc08b2c9f18073f3f
Deepika Gonuguntla <>
2023-11-30 16:57:38 -0500

    Added scools

--- db/migrate/20231130215302_add_december_2023_schools.rb
+++ db/migrate/20231130215302_add_december_2023_schools.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddDec2023Schools < ActiveRecord::Migration[6.1]
+class AddDecember2023Schools < ActiveRecord::Migration[6.1]
   def up
     Rake::Task['ingest:import_all_nyc_schools'].invoke('data/public/December_2023_schools_in_mln.csv', true)
     Rake::Task['ingest_sierra_data:import_sierra_codes_and_zcodes'].invoke('data/public/December_2023_sierra_codes_in_mln.csv', true)
