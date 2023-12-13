1384af01cc74f9ae18567b011214335e0c65508f
Deepika Gonuguntla <>
2023-12-13 09:37:43 -0500

    Added schools in MLN

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20231213021708) do
+ActiveRecord::Schema.define(version: 20231213143102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
