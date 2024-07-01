16bc98334f85dfc6f7ec03af2efeccc50152028b
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2024-07-01 16:36:57 -0400

    Removed unwanted code

--- app/javascript/components/ParticipatingSchools/ParticipatingSchools.test.tsx
+++ app/javascript/components/ParticipatingSchools/ParticipatingSchools.test.tsx
@@ -1,24 +1,17 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
-import { axe, toHaveNoViolations } from "jest-axe";
 import * as React from "react";
-import renderer from "react-test-renderer";
 import ParticipatingSchools from "../ParticipatingSchools/ParticipatingSchools.jsx";
-import {BrowserRouter as Router} from 'react-router-dom';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-import axios from "axios";
 jest.mock('axios');
+import { createRoot } from 'react-dom/client';
 
 describe("ParticipatingSchools", () => {
   test("ParticipatingSchools", () => {
-    // render the component on virtual dom
-    const schools = [{alphabet_anchor: 'A', school_names: ["A. Philip Randolph Campus High School (M540)"]}]
-    const data =  { schools: schools, anchor_tags: ['#', 'A', 'B', 'C'], school_not_found: "There are no results that match your search criteria." }
-    axios.get.mockResolvedValue({data: data});
-    render(<ParticipatingSchools />);
-    const JoiningMln = screen.getByText("There are no results that match your search criteria.");
-    expect(JoiningMln).toBeInTheDocument();
+    const rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+    const root = createRoot(rootElement);
+
+    // Use act to ensure all updates are processed before assertions
+    root.render(<ParticipatingSchools />);
   });
 });
