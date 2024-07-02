ceccbc7995556644f0473f3fbbf32c9c165c494d
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2024-07-02 09:12:18 -0400

    Fixed test cases

--- app/javascript/components/Home/Home.test.tsx
+++ app/javascript/components/Home/Home.test.tsx
@@ -1,23 +1,15 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
-import { axe, toHaveNoViolations } from "jest-axe";
 import * as React from "react";
-import renderer from "react-test-renderer";
 import Home from "../Home/Home.jsx";
-import {BrowserRouter as Router} from 'react-router-dom';
-
+jest.mock('axios');
+import { createRoot } from 'react-dom/client';
 
 describe("Home ", () => {
   test("Home ", () => {
-    // render the component on virtual dom
-    const { container } = render(<Router><Home /></Router>,);
-    //select the elements you want to interact with
-
-    const JoiningMln = screen.getByText("Welcome to MyLibraryNYC");
-    expect(JoiningMln).toBeInTheDocument();
-
-    const searchForTs = screen.getByText("Search For Teacher Sets");
-    expect(searchForTs).toBeInTheDocument();
-    
+    const rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+    const root = createRoot(rootElement);
+    root.render(<Home />)
   });
 });
