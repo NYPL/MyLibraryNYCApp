940b2ec39637fa3500114d7d3d0a48d32cbc12f6
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2024-07-05 14:10:29 -0400

    Added colors

--- app/javascript/components/BookTitles.jsx
+++ app/javascript/components/BookTitles.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {
+  useColorModeValue,
+  Link,
+} from "@nypl/design-system-react-components";
 
 const BookTitles = ({ books }) => {
-  const bookTitles = (books) => {
-    console.log(books); // Log books to debug
+  const titleColor = useColorModeValue(
+    "var(--nypl-colors-ui-link-primary)",
+    "var(--nypl-colors-dark-ui-link-primary)"
+  );
 
+  const bookTitles = (books) => {
     if (books && books.length > 0) {
       return (
         <>
@@ -12,15 +19,16 @@ const BookTitles = ({ books }) => {
             {books.map((book, i) =>
               book.title !== null ? (
                 <li key={"ts-books-key-" + i}>
-                  <a
+                  <Link
                     id={"ts-books-" + i}
                     href="#"
                     onClick={(e) => {
                       windowScrollTop(e, book);
                     }}
+                    color={titleColor}
                   >
                     {book.title}
-                  </a>
+                  </Link>
                 </li>
               ) : null
             )}
