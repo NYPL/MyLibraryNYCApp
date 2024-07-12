77d9636e44e311919cc253c0745311e0f15d72d3
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2024-07-12 12:50:01 -0400

    Updated readme document for local setup

--- app/javascript/components/SignIn/SignIn.test.tsx
+++ app/javascript/components/SignIn/SignIn.test.tsx
@@ -5,20 +5,17 @@ import { createRoot } from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
 
 describe("SignIn", () => {
-  describe('Faq component', () => {
+  afterEach(() => {
+    // Clear all mocks after each test
+    jest.clearAllMocks();
+  });
 
-    afterEach(() => {
-      // Clear all mocks after each test
-      jest.clearAllMocks();
-    });
-
-    test("SignIn", () => {
-      // render the component on virtual dom
-      const rootElement = document.createElement('div');
-      document.body.appendChild(rootElement);
-      const root = createRoot(rootElement);
-      // Use act to ensure all updates are processed before assertions
-      root.render(<SignIn />);
-    });
+  test("SignIn", () => {
+    // render the component on virtual dom
+    const rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+    const root = createRoot(rootElement);
+    // Use act to ensure all updates are processed before assertions
+    root.render(<SignIn />);
   });
 });
