71050ad0166732e52666a7c17cc2126b5b4007bf
Jeff Croyle <jeffreycroyle@nypl.org>
2024-11-22 16:13:33 -0500

    use port 443 for es client

--- app/models/elastic_search.rb
+++ app/models/elastic_search.rb
@@ -17,6 +17,7 @@ class ElasticSearch
 
     arguments = {
       host: es_host(@es_config),
+      port: 443,
       transport_options: {
         request: { open_timeout: @es_config['connect_timeout'] },
         headers: { content_type: 'application/json' }
