07f9de24662f8cadd6f266ca0002e9b17d5527e2
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2025-01-03 15:08:29 -0500

    Changed signin message

--- app/javascript/components/Faqs/Faqs.jsx
+++ app/javascript/components/Faqs/Faqs.jsx
@@ -58,8 +58,8 @@ export default function Faqs(props) {
       breakout={<AppBreadcrumbs />}
       contentTop={
         <>
-          {renderInactiveSchoolMessage(isSchoolActive)}
           {<SignedInMsg signInDetails={props} />} 
+          {renderInactiveSchoolMessage(isSchoolActive)}
         </>}
       contentPrimary={
         <>
