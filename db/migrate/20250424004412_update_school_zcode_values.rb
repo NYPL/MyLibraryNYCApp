6e4ec705e96d28ea9688c3c5c6752ad19361759e
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2025-04-23 20:45:42 -0400

    Update school code values

--- db/migrate/20250424004412_update_school_zcode_values.rb
+++ db/migrate/20250424004412_update_school_zcode_values.rb
@@ -1,4 +1,4 @@
-class UpdateSchoolZcodes < ActiveRecord::Migration[7.2]
+class UpdateSchoolZcodeValues < ActiveRecord::Migration[7.2]
   def up
     Rake::Task["ingest_sierra_data:update_sierra_zcodes"].invoke("data/public/April_2025_update_school_zcodes.csv", true)
   end
