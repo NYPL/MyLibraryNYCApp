3c4f19183f13d5298ff411968bd41f05d01c8f76
Deepika Gonuguntla <deepikagonuguntla@nypl.org>
2025-05-18 12:07:18 -0400

    reverted code

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,8 @@ class User < ActiveRecord::Base
   # Makes getters and setters
   attr_accessor :password
 
-  validates_numericality_of :barcode, on: :create, presence: true, allow_blank: true, only_integer: true, uniqueness: true
-  validates_numericality_of :barcode, on: :update, presence: true, allow_blank: true, only_integer: true, uniqueness: true
+  validates_numericality_of :barcode, on: :create, presence: true, allow_blank: false, only_integer: true, less_than_or_equal_to: 27777099999999, uniqueness: true
+  validates_numericality_of :barcode, on: :update, presence: true, allow_blank: false, only_integer: true, less_than_or_equal_to: 27777099999999, uniqueness: true
 
   # Validation's for email and pin only occurs when a user record is being
   # created on sign up. Does not occur when updating
--- lib/user_delayed_job.rb
+++ lib/user_delayed_job.rb
@@ -3,47 +3,56 @@
 class UserDelayedJob < Struct.new(:user_id, :pin)
   def perform
     user = User.find(user_id)
+
     if user.blank?
-      raise Exceptions::ArgumentError, "UserJob called with nil user or pin."
+      defensive_log("UserBarcodeJob called with nil user or pin.")
+      raise Exceptions::ArgumentError, "UserBarcodeJob called with nil user or pin."
     end
 
-    is_username_available = false
+    is_barcode_available = false
     number_tries = 0
-    # username already found in Sierra for another user?
-    # well, we can't be useing this user with a duplicate username.
-    # ask the user to generate another username, and try again.
-    while is_username_available == false && number_tries < 7
-      is_username_available, user_name = user.username_available_in_sierra
-      defensive_log("#{self.class.name}: userName: #{user_name} isUsernameAvailable: #{is_username_available} numberOfRetries: #{number_tries}")
-      # userName is not available in sierra, call sierra again with latest userName
-      unless is_username_available
+    # barcode already found in Sierra for another user?
+    # well, we can't be saving this user with a duplicate barcode.
+    # ask the user to increment the barcode, and try again.
+    while is_barcode_available == false && number_tries < 7
+      begin
         number_tries += 1
-        defensive_log("#{self.class.name}: userName #{user_name}] was already in Sierra,
-          calling user sierra again. No.of retries number_tries #{number_tries}")
-        is_username_available, user_name = user.username_available_in_sierra
+        is_barcode_available = user.barcode_available_in_sierra?
+        # wait a bit before hitting Sierra up again
+      rescue Exceptions::InvalidResponse => e
+        defensive_log("#{self.class.name}: user.check_barcode_uniqueness_with_sierra threw an error: #{e.message || "nil"}")
+        raise e
+      end
+
+      # Barcode is not available in sierra assign barcode to user
+      # and call sierra again with latest barcode
+      unless is_barcode_available
+        defensive_log("#{self.class.name}: barcode [#{user.barcode}] was already in Sierra,
+          calling user assign_barcode again. No.of retries number_tries #{number_tries}")
+        user.assign_barcode(number_tries)
         sleep(60)
       end
     end
-    if is_username_available == true
-      begin
-        defensive_log("#{self.class.name}: userName [#{user_name}] is available in Sierra, calling patron creator service.")
-        response = user.invoke_patron_create_service(pin, user_name)
-        defensive_log("Response code: #{response.code}")
 
-        if response.code == 200
+    if is_barcode_available == true
+      begin
+        defensive_log("#{self.class.name}: barcode [#{user.barcode}] is available in Sierra, calling patron creator service.")
+        response = user.send_request_to_patron_creator_service(pin)
+
+        if response.code == 201
           defensive_log("Patron created successfully")
           # Send user account confirmation email
           user.account_confirmed_email_to_user
           defensive_log("#{self.class.name}: Patron creator service ran. Saving user in MLN db.")
           user.save_as_complete!
         end
-      rescue Exceptions::InvalidResponse, Exceptions::StandardError => e
+      rescue Exceptions::InvalidResponse => e
         defensive_log("#{self.class.name}: \
-          invoke_patron_create_service or threw: #{e.message || "nil"} #{response}")
+          send_request_to_patron_creator_service or user.save_as_complete threw: #{e.message || "nil"}")
         raise e
       end
     else
-      defensive_log("#{self.class.name}: UserJob.perform: username_already_in_sierra")
+      defensive_log("#{self.class.name}: UserBarcodeJob.perform: barcode_already_in_sierra still true")
     end
   end
 
