a777b6a7b4629f73e7cf0437c824263d9786d981
Russell Cullen <russellcullen@nypl.org>
2025-10-30 12:48:03 -0400

    Use syndetics for covers and isbn directly

--- app/assets/javascripts/active_admin_custom.js
+++ app/assets/javascripts/active_admin_custom.js
@@ -104,7 +104,7 @@ function activateSchool(schoolId, activate) {
   
       $.each(el.data('titles'), function(i, t) {
         if(t['isbns'])
-          image_uris[t['id']] = 'http://contentcafe2.btol.com/ContentCafe/Jacket.aspx?&userID=NYPL49807&password=CC68707&content=M&Return=1&Type=S&Value=' + t['isbns'][0];
+          image_uris[t["id"]] = "https://secure.syndetics.com/index.aspx?isbn=" + t["isbns"][0] + "/SC.gif&client=nyplvega&type=hw7";
         title_uris[t['id']] = t['details_url']
       });
       // console.log("set image uri: ", image_uris, title_uris);
